6e3a101dcbccb093ee50e39daf485fb116126600
WanderWang <wander.wang@ismole.com>
2014-03-27 17:58:54 +0800

    删除HelloWorld

--- template/project_creator/game_file_list.js
+++
@@ -1,8 +0,0 @@
-var game_file_listPrefix = "../game/src/";
-
-game_file_list =
-    [
-        "GameLayer.js"
-    ].map(function (item) {
-            return game_file_listPrefix + item;
-        });
