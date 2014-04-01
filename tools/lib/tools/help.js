e433339034d9f9bae9d5d30a1873dc3ecca40029
yjtx <friendhhy@163.com>
2014-04-01 16:34:49 +0800

    修改 帮助 创建命令提示
    修改 帮助 创建命令提示

--- tools/lib/tools/help.js
+++ tools/lib/tools/help.js
@@ -2,7 +2,7 @@
 function run(currDir, args, opts) {
     
     console.log("//创建一个新工程");
-    console.log("egret c|create proj-name [-a] [-u url]");
+    console.log("egret c|create proj-name [-e engineUrl] [-u url]");
     console.log("//删除一个工程");
     console.log("egret r|remove proj-name [-u url]");
     console.log("//构建一个工程");
