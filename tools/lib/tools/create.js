166482aacd60570fdbec5dafa98a40a2e57b80e1
WanderWang <wander.wang@ismole.com>
2014-04-04 16:29:00 +0800

    创建项目添加检查

--- tools/lib/tools/create.js
+++ tools/lib/tools/create.js
@@ -29,18 +29,29 @@ function run(currentDir, args, opts) {
 
     generateConfigJson(currentDir,engine,projectName);
 
+
+    copyEngine(currentDir,engine);
+
+    var projPath = path.join(currentDir, projectName);
+    //创建 游戏目录
+    libs.copy(path.join(source, "game"), projPath);
+    console.log("创建成功!");
+}
+
+
+function copyEngine(currentDir,engine){
     //创建 引擎目录
     if (engine && engine.length > 0) {
         var engine_root = path.join(currentDir, "egret");
+        if (fs.existsSync(engine_root)){
+            console.error("当前目录下已存在引擎文件夹");
+            process.exit(1);
+        }
         fs.mkdirSync(engine_root);
         var target_src = path.join(engine_root, "src");
         var source_src = path.join(engine[0], "src");
         libs.copy(source_src, target_src);
     }
-    var projPath = path.join(currentDir, projectName);
-    //创建 游戏目录
-    libs.copy(path.join(source, "game"), projPath);
-    console.log("创建成功!");
 }
 
 /**
@@ -54,10 +65,20 @@ function generateConfigJson(currentDir,engine,projectName) {
     if (!fs.existsSync(configPath)) {
         var gameData = {};
         gameData.game = {};
+        gameData.engine = "";
     }
     else {
         var txt = fs.readFileSync(configPath, "utf8");
-        var gameData = JSON.parse(txt);
+        try{
+            var gameData = JSON.parse(txt);
+        }
+        catch(e){
+            console.log ("config.json解析失败，重新生成...");
+            var gameData = {};
+            gameData.game = {};
+            gameData.engine = "";
+        }
+
     }
     gameData["game"][projectName] = projectName + "/";
     if (engine && engine.length > 0) {
