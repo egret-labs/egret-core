226d2621372749e6d6619db4d35b8054bbf1d849
DOM <dom@domlib.com>
2014-04-17 20:48:27 +0800

    SkinnableComponent完成

---
+++ src/extension/gui/components/SkinnableComponent.ts
@@ -0,0 +1,381 @@
+/**
+ * Copyright (c) Egret-Labs.org. Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/// <reference path="../../../egret/display/DisplayObject.ts"/>
+/// <reference path="../../../egret/events/Event.ts"/>
+/// <reference path="../../../egret/core/Injector.ts"/>
+/// <reference path="supportClasses/SkinBasicLayout.ts"/>
+/// <reference path="../core/ISkin.ts"/>
+/// <reference path="../core/IStateClient.ts"/>
+/// <reference path="../events/SkinPartEvent.ts"/>
+
+module ns_egret {
+
+	export class SkinnableComponent extends UIAsset{
+		/**
+		 * 构造函数
+		 */		
+		public constructor(){
+			super();
+			this.touchChildren = true;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public createChildren():void{
+			if(this.skinName==null){//让部分组件在没有皮肤的情况下创建默认的子部件。
+				this.onGetSkin(null,null);
+			}
+			super.createChildren();
+		}
+
+		private _skinObject:any
+		/**
+		 * 存储皮肤适配器解析skinName得到的原始皮肤对象，包括非显示对象皮肤的实例。
+		 */
+		public get skinObject():any{
+			return this._skinObject;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public onGetSkin(skin:any,skinName:any):void{
+			var oldSkin:any = this._skinObject;
+			this.detachSkin(oldSkin);
+			if(this._skin){
+				if(this._skin.parent==this){
+					this.removeFromDisplayList(this._skin); 
+				}
+			}
+			
+			if(skin instanceof DisplayObject){
+				this._skin = <DisplayObject> skin;
+				this.addToDisplayListAt(this._skin,0);
+			}
+			else{
+				this._skin = null;
+			}
+			this._skinObject = skin;
+			this.attachSkin(this._skinObject);
+			this.aspectRatio = NaN;
+			this.invalidateSkinState();
+			this.invalidateSize();
+			this.invalidateDisplayList();
+		}
+		
+		/**
+		 * 附加皮肤
+		 */		
+		public attachSkin(skin:any):void{
+			if(skin instanceof ISkin){
+				var newSkin:ISkin = <ISkin> skin;
+				newSkin.hostComponent = this;
+				this.findSkinParts();
+			}
+			else{
+				if(!this.hasCreatedSkinParts){
+					this.createSkinParts();
+					this.hasCreatedSkinParts = true;
+				}
+			}
+			if(skin instanceof ISkin&&skin instanceof DisplayObject)
+				this.skinLayoutEnabled = false;
+			else
+				this.skinLayoutEnabled = true;
+		}
+		/**
+		 * 匹配皮肤和主机组件的公共变量，并完成实例的注入。此方法在附加皮肤时会自动执行一次。
+		 * 若皮肤中含有延迟实例化的子部件，在子部件实例化完成时需要从外部再次调用此方法,完成注入。
+		 */	
+		public findSkinParts():void{
+			var curSkin:any = this._skinObject;
+            if(curSkin&&"skinParts" in curSkin.prototype){
+                var skinParts:Array = curSkin.prototype["skinParts"];
+                var length:number = skinParts.length;
+                for(var i:number=0;i<length;i++){
+                    var partName:string = skinParts[i];
+                    if((partName in this)&&(partName in curSkin)&&curSkin[partName] != null
+                        &&this[partName]==null){
+                        try{
+                            this[partName] = curSkin[partName];
+                            this.partAdded(partName,curSkin[partName]);
+                        }
+                        catch(e){
+                        }
+                    }
+                }
+            }
+
+
+
+		}
+		
+		/**
+		 * 由组件自身创建了SkinPart的标志
+		 */		
+		private hasCreatedSkinParts:boolean = false;
+		/**
+		 * 由组件自身来创建必要的SkinPart，通常是皮肤为空或皮肤不是ISkinPart时调用。
+		 */		
+		public createSkinParts():void{
+		}
+		/**
+		 * 删除组件自身创建的SkinPart
+		 */		
+		public removeSkinParts():void{
+		}
+		
+		/**
+		 * 卸载皮肤
+		 */		
+		public detachSkin(skin:any):void{       
+			if(this.hasCreatedSkinParts){
+				this.removeSkinParts();
+				this.hasCreatedSkinParts = false;
+			}
+            if(skin&&"skinParts" in skin.prototype){
+                var skinParts:Array = skin.prototype["skinParts"];
+                var length:number = skinParts.length;
+				for(var i:number=0;i<length;i++){
+                    var partName:string = skinParts[i];
+					if(!(partName in this))
+						continue;
+					if (this[partName] != null){
+						this.partRemoved(partName,this[partName]);
+					}
+					this[partName] = null;
+				}
+				(<ISkin> skin).hostComponent = null;
+			}
+		}
+		
+		/**
+		 * 若皮肤是ISkinPartHost,则调用此方法附加皮肤中的公共部件
+		 */		
+		public partAdded(partName:string,instance:any):void{
+			var event:SkinPartEvent = new SkinPartEvent(SkinPartEvent.PART_ADDED);
+			event.partName = partName;
+			event.instance = instance;
+			this.dispatchEvent(event);
+		}
+		/**
+		 * 若皮肤是ISkinPartHost，则调用此方法卸载皮肤之前注入的公共部件
+		 */		
+		public partRemoved(partName:string,instance:any):void{       
+			var event:SkinPartEvent = new SkinPartEvent(SkinPartEvent.PART_REMOVED);
+			event.partName = partName;
+			event.instance = instance;
+			this.dispatchEvent(event);
+		}
+		
+		
+		
+		//========================皮肤视图状态=====================start=======================
+		
+		private stateIsDirty:boolean = false;
+		
+		/**
+		 * 标记当前需要重新验证皮肤状态
+		 */		
+		public invalidateSkinState():void{
+			if (this.stateIsDirty)
+				return;
+			
+			this.stateIsDirty = true;
+			this.invalidateProperties();
+		}
+		
+		/**
+		 * 子类覆盖此方法,应用当前的皮肤状态
+		 */		
+		public validateSkinState():void{
+			var curState:string = this.getCurrentSkinState();
+			var hasState:boolean = false;
+			var curSkin:any = this._skinObject;
+			if(curSkin instanceof IStateClient){
+				(<IStateClient> curSkin).currentState = curState;
+				hasState = (<IStateClient> curSkin).hasState(curState);
+			}
+			if(this.hasEventListener("stateChanged"))
+				this.dispatchEvent(new Event("stateChanged"));
+		}
+		
+		private _autoMouseEnabled:boolean = true;
+		/**
+		 * 在enabled属性发生改变时是否自动开启或禁用鼠标事件的响应。默认值为true。
+		 */
+		public get autoTouchEnabled():boolean{
+			return this._autoMouseEnabled;
+		}
+		
+		public set autoTouchEnabled(value:boolean):void{
+			if(this._autoMouseEnabled==value)
+				return;
+			this._autoMouseEnabled = value;
+			if(this._autoMouseEnabled){
+				super.touchChildren = this.enabled ? this.explicitMouseChildren : false;
+				super.touchEnabled  = this.enabled ? this.explicitMouseEnabled  : false;
+			}
+			else{
+				super.touchChildren = this.explicitMouseChildren;
+				super.touchEnabled  = this.explicitMouseEnabled;
+			}
+		}
+		
+		/**
+		 * 外部显式设置的mouseChildren属性值 
+		 */		
+		private explicitMouseChildren:boolean = true;
+		/**
+		 * @inheritDoc
+		 */		
+		public set touchChildren(value:boolean):void{
+			if(this.enabled)
+				super.touchChildren = value;
+			this.explicitMouseChildren = value;
+		}
+		/**
+		 * 外部显式设置的mouseEnabled属性值
+		 */		
+		private explicitMouseEnabled:boolean = true;
+		/**
+		 * @inheritDoc
+		 */	
+		public set touchEnabled(value:boolean):void{
+			if(this.enabled)
+				super.touchEnabled = value;
+			this.explicitMouseEnabled = value;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public set enabled(value:boolean):void{
+			if(super.enabled==value)
+				return;
+			super.enabled = value;
+			if(this._autoMouseEnabled){
+				super.touchChildren = value ? this.explicitMouseChildren : false;
+				super.touchEnabled  = value ? this.explicitMouseEnabled  : false;
+			}
+			this.invalidateSkinState();
+		}
+		
+		/**
+		 * 返回组件当前的皮肤状态名称,子类覆盖此方法定义各种状态名
+		 */		
+		public getCurrentSkinState():string {
+			return this.enabled?"normal":"disabled"
+		}
+		
+		//========================皮肤视图状态===================end========================
+		
+		/**
+		 * @inheritDoc
+		 */
+		public commitProperties():void{
+			super.commitProperties();
+			if(this.stateIsDirty){
+				this.stateIsDirty = false;
+				this.validateSkinState();
+			}
+		}
+		
+		private layout:SkinBasicLayout;
+		/**
+		 * 启用或禁用组件自身的布局。通常用在当组件的皮肤不是ISkinPartHost，又需要自己创建子项并布局时。
+		 */		
+		public set skinLayoutEnabled(value:boolean):void{
+			var hasLayout:boolean = (this.layout != null);
+			if(hasLayout==value)
+				return;
+			if(value){
+				this.layout = new SkinBasicLayout();
+				this.layout.target = this;
+			}
+			else{
+				this.layout.target = null;
+ 				this.layout = null;
+			}
+			this.invalidateSize();
+			this.invalidateDisplayList();
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public childXYChanged():void{
+			if(this.layout){
+				this.invalidateSize();
+				this.invalidateDisplayList();
+			}
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public measure():void{
+			super.measure();
+			if(this.layout){
+				this.layout.measure();
+			}
+			var skinObject:any = this._skinObject;
+			if(!this._skin&&skinObject){//为非显示对象的皮肤测量
+				var measuredW:number = this.measuredWidth;
+				var measuredH:number = this.measuredHeight;
+				try{
+					if(!isNaN(skinObject.width))
+						measuredW = Math.ceil(skinObject.width);
+					if(!isNaN(skinObject.height))
+						measuredH = Math.ceil(skinObject.height);
+					if(skinObject.hasOwnProperty("minWidth")&&
+						measuredW<skinObject.minWidth){
+						measuredW = skinObject.minWidth;
+					}
+					if(skinObject.hasOwnProperty("maxWidth")&&
+						measuredW>skinObject.maxWidth){
+						measuredW = skinObject.maxWidth;
+					}
+					if(skinObject.hasOwnProperty("minHeight")&&
+						measuredH<skinObject.minHeight){
+						measuredH = skinObject.minHeight;
+					}
+					if(skinObject.hasOwnProperty("maxHeight")&&
+						measuredH>skinObject.maxHeight){
+						measuredH = skinObject.maxHeight
+					}
+					this.measuredWidth = measuredW;
+					this.measuredHeight = measuredH;
+				}
+				catch(e:Error){}
+			}
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public updateDisplayList(unscaledWidth:number, unscaledHeight:number):void{
+			super.updateDisplayList(unscaledWidth,unscaledHeight);
+			if(this.layout){
+				this.layout.updateDisplayList(unscaledWidth,unscaledHeight);
+			}
+		}
+	}
+}
---
+++ src/extension/gui/components/supportClasses/SkinBasicLayout.ts
@@ -0,0 +1,182 @@
+/**
+ * Copyright (c) Egret-Labs.org. Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/// <reference path="../SkinnableComponent.ts"/>
+/// <reference path="../../core/ILayoutElement.ts"/>
+
+module ns_egret {
+
+	export class SkinBasicLayout{
+		public constructor(){
+			super();
+		}
+		
+		private _target:SkinnableComponent;
+
+		/**
+		 * 目标布局对象
+		 */		
+		public get target():SkinnableComponent{
+			return this._target;
+		}
+
+		public set target(value:SkinnableComponent):void{
+			this._target = value;
+		}
+
+		
+		/**
+		 * 测量组件尺寸大小
+		 */		
+		public measure():void{
+			if (this.target==null)
+				return;
+			
+			var measureW:number = 0;
+			var measureH:number = 0;
+
+            var skin:any = this._target.skin;
+			var count:number = this.target.numChildren;
+			for(var i:number = 0; i < count; i++){
+				var layoutElement:ILayoutElement = <ILayoutElement> this.target.getChildAt(i);
+				if (!layoutElement||layoutElement==skin||!layoutElement.includeInLayout)
+					continue;
+				
+				var hCenter:number   = layoutElement.horizontalCenter;
+				var vCenter:number   = layoutElement.verticalCenter;
+				var left:number      = layoutElement.left;
+				var right:number     = layoutElement.right;
+				var top:number       = layoutElement.top;
+				var bottom:number    = layoutElement.bottom;
+				
+				var extX:number;
+				var extY:number;
+				
+				if (!isNaN(left) && !isNaN(right)){
+					extX = left + right;                
+				}
+				else if (!isNaN(hCenter)){
+					extX = Math.abs(hCenter) * 2;
+				}
+				else if (!isNaN(left) || !isNaN(right)){
+					extX = isNaN(left) ? 0 : left;
+					extX += isNaN(right) ? 0 : right;
+				}
+				else{
+					extX = layoutElement.preferredX;
+				}
+				
+				if (!isNaN(top) && !isNaN(bottom)){
+					extY = top + bottom;                
+				}
+				else if (!isNaN(vCenter)){
+					extY = Math.abs(vCenter) * 2;
+				}
+				else if (!isNaN(top) || !isNaN(bottom)){
+					extY = isNaN(top) ? 0 : top;
+					extY += isNaN(bottom) ? 0 : bottom;
+				}
+				else{
+					extY = layoutElement.preferredY;
+				}
+				
+				var preferredWidth:number = layoutElement.preferredWidth;
+				var preferredHeight:number = layoutElement.preferredHeight;
+				
+				measureW = Math.ceil(Math.max(measureW, extX + preferredWidth));
+				measureH = Math.ceil(Math.max(measureH, extY + preferredHeight));
+			}
+			
+			this.target.measuredWidth = Math.max(measureW,this.target.measuredWidth);
+			this.target.measuredHeight = Math.max(measureH,this.target.measuredHeight);
+		}
+		
+		/**
+		 * 更新显示列表
+		 */	
+		public updateDisplayList(unscaledWidth:number, unscaledHeight:number):void{
+			if (this.target==null)
+				return;
+			
+			var count:number = this.target.numChildren;
+            var skin:any = this._target.skin;
+			for(var i:number = 0; i < count; i++){
+				var layoutElement:ILayoutElement = <ILayoutElement> this.target.getChildAt(i);
+				if (layoutElement==null||layoutElement==skin||!layoutElement.includeInLayout)
+					continue;
+				
+				var hCenter:number          = layoutElement.horizontalCenter;
+				var vCenter:number          = layoutElement.verticalCenter;
+				var left:number             = layoutElement.left;
+				var right:number            = layoutElement.right;
+				var top:number              = layoutElement.top;
+				var bottom:number           = layoutElement.bottom;
+				var percentWidth:number     = layoutElement.percentWidth;
+				var percentHeight:number    = layoutElement.percentHeight;
+				
+				var childWidth:number = NaN;
+				var childHeight:number = NaN;
+				
+				
+				if(!isNaN(left) && !isNaN(right)){
+					childWidth = unscaledWidth - right - left;
+				}
+				else if (!isNaN(percentWidth)){
+					childWidth = Math.round(unscaledWidth * Math.min(percentWidth * 0.01, 1));
+				}
+				
+				if (!isNaN(top) && !isNaN(bottom)){
+					childHeight = unscaledHeight - bottom - top;
+				}
+				else if (!isNaN(percentHeight)){
+					childHeight = Math.round(unscaledHeight * Math.min(percentHeight * 0.01, 1));
+				}
+				
+				layoutElement.setLayoutBoundsSize(childWidth, childHeight);
+				
+				var elementWidth:number = layoutElement.layoutBoundsWidth;
+				var elementHeight:number = layoutElement.layoutBoundsHeight;
+				
+				
+				var childX:number = NaN;
+				var childY:number = NaN;
+				
+				if (!isNaN(hCenter))
+					childX = Math.round((unscaledWidth - elementWidth) / 2 + hCenter);
+				else if (!isNaN(left))
+					childX = left;
+				else if (!isNaN(right))
+					childX = unscaledWidth - elementWidth - right;
+				else
+					childX = layoutElement.layoutBoundsX;
+				
+				if (!isNaN(vCenter))
+					childY = Math.round((unscaledHeight - elementHeight) / 2 + vCenter);
+				else if (!isNaN(top))
+					childY = top;
+				else if (!isNaN(bottom))
+					childY = unscaledHeight - elementHeight - bottom;
+				else
+					childY = layoutElement.layoutBoundsY;
+				
+				layoutElement.setLayoutBoundsPosition(childX, childY);
+			}
+		}
+		
+	}
+}
