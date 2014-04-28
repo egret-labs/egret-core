19341aeadd0e94a0f870782d4ce5ef2d1937b392
DOM <dom@domlib.com>
2014-04-28 14:47:40 +0800

    HashObject.hashCode不需要默认值。

--- src/egret/core/HashObject.ts
+++ src/egret/core/HashObject.ts
@@ -37,7 +37,7 @@ module ns_egret {
          */
         private static hashCount:number = 1;
 
-        private _hashCode:number = 1;
+        private _hashCode:number;
         /**
          * 返回此对象唯一的哈希值,用于唯一确定一个对象。
          * @method ns_egret.HashObject#hashCode
