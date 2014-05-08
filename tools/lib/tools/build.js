8f706595918677f3e6c65ffcb96fbad95797cd8a
WanderWang <wander.wang@ismole.com>
2014-05-08 14:29:37 +0800

    构建脚本优化
    构建脚本优化

--- tools/lib/tools/build.js
+++ tools/lib/tools/build.js
@@ -9,39 +9,42 @@ var param = require("../core/params_analyze.js");
 var compiler = require("./compile.js")
 var currDir_global;
 function run(currDir, args, opts) {
-    var u = opts["-u"];
-    if (u && u.length > 0) {
-        currDir = u[0];
-    }
     currDir_global = currDir;
+    var needCompileEngine = opts["-e"];
 
     var game_path = args[0];
     if (!game_path) {
         libs.exit(1101);
     }
 
-    var tasks = [
-        function (callback) {
-            compiler.compile(path.join(param.getEgretPath(), "src"), path.join(currDir, game_path, "bin-debug/lib"), callback);
-        },
+    var task = [];
+    if (needCompileEngine) {
+        task.push(
+            function (callback) {
+                compiler.compile(path.join(param.getEgretPath(), "src"), path.join(currDir, game_path, "bin-debug/lib"), callback);
+            }
+        );
+    }
 
+    task.push(
         function (callback) {
             compiler.compile(path.join(currDir, game_path, "src"), path.join(currDir, game_path, "bin-debug/src"), callback);
         }
-    ];
+    )
 
-    async.series(tasks,function(err){
-        console.log ("编译成功");
+
+    async.series(task, function (err) {
+        console.log("编译成功");
     })
 }
 
 
-function help_title(){
+function help_title() {
     return "编译指定项目";
 }
 
 
-function help_example(){
+function help_example() {
     return "egret build [project_name]";
 }
 
