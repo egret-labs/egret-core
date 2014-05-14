43c9e9cd528629832d4c699d90c0d2776c6e34e8
DOM <dom@domlib.com>
2014-05-14 20:15:18 +0800

    实现getQualifiedClassName()返回命名空间

--- src/egret/utils/getQualifiedClassName.ts
+++ src/egret/utils/getQualifiedClassName.ts
@@ -16,40 +16,87 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-module ns_egret {
+/// <reference path="getTimer.ts"/>
 
+module ns_egret {
+    /**
+     * 在window上需要读取的命名空间属性列表
+     */
+    export var __moduleNameList:Array<string> = ["ns_egret","dragonBones"];
+    /**
+     * 需要重新刷新类名的标志
+     */
+    export var __invalidateModuleFlag:boolean = true;
+
+    var __functionNameCache:any = {};
     /**
      * 返回一个对象的完全限定名<br/>
      * @param value 需要完全限定类名称的对象，可以将任何 TypeScript / JavaScript值传递给此方法，包括所有可用的TypeScript / JavaScript类型、对象实例、原始类型（如number）和类对象
      * @returns {string} 包含完全限定类名称的字符串<br />
-     * 在Flash中，此方法返回的是类的全名，如 Bitmap对象返回 flash.display.Bitmap <br/>
-     * 在Egret中，由于JavaScript语言自身的限制，Bitmap对象返回 Bitmap而非 ns_egret.Bitmap <br/r>
      * @example
-     *  ns_egret.getQualifiedClassName(ns_egret.DisplayObject) //返回 DisplayObject
-     *  ns_egret.getQualifiedClassName(new ns_egret.DisplayObject()) //返回 DisplayObject
-     *  ns_egret.getQualifiedClassName(19910901) //返回 Number
-     *  ns_egret.getQualifiedClassName("Hello,Egret") //返回 String
+     *  ns_egret.getQualifiedClassName(ns_egret.DisplayObject) //返回 "ns_egret.DisplayObject"
      */
-    export function getQualifiedClassName(value:any) {
-        var constructorFunction;
-        var constructorString:string;
-        if (value.prototype) {
-            constructorFunction = value.prototype.constructor;
+    export function getQualifiedClassName(value:any):string {
+        var constructorFunction:any = value.prototype ? value.prototype.constructor : value.__proto__.constructor;
+        if(constructorFunction.__class__){
+            return constructorFunction.__class__;
         }
-        else {
-            constructorFunction = value.__proto__.constructor;
+        if(__invalidateModuleFlag){
+            updateModules();
+            __invalidateModuleFlag = false;
+            if(constructorFunction.__class__){
+                return constructorFunction.__class__;
+            }
         }
-        if (!__getQualifiedClassName__cache[constructorFunction]) {
-            constructorString = constructorFunction.toString();
-            var index:number = constructorString.indexOf("(");
-            var result:string = constructorString.substring(9, index);
-            __getQualifiedClassName__cache[constructorFunction] = result;
+        var key:string = constructorFunction.toString();
+        var className:string = __functionNameCache[key];
+        if(!className){
+            __functionNameCache[key] = className = getFunctionName(constructorFunction);
         }
-        return __getQualifiedClassName__cache[constructorFunction]
+        return className;
     }
 
+    function getFunctionName(constructorFunction:Function):string{
+        var constructorString:string = constructorFunction.toString();
+        var index:number = constructorString.indexOf("(");
+        return constructorString.substring(9, index);
+    }
 
-    var __getQualifiedClassName__cache:Object = {};
+    function updateModules():void{
+//      var t:number = getTimer();
+        var list:Array<string> = __moduleNameList;
+        var length:number = list.length;
+        for(var i:number=0;i<length;i++){
+            var key:string = list[i];
+            var value:any = window[key];
+            if(value&&typeof(value)=="object"&&value.__proto__&&
+                getFunctionName(value.__proto__.constructor)=="Object"){
+                setModuleName(value,key);
+            }
+        }
+
+//        t = getTimer()-t;
+//        console.log("updateModules: "+t+"ms");
+    }
+
+    function setModuleName(ns:any,name:string):void{
+        for(var key in ns){
+            var value:any = ns[key];
+            var type:string = typeof(value);
+            if(type=="function"){
+                if(!value.prototype){
+                    continue;
+                }
+                var func:any = value.prototype.constructor;
+                if(!func.__class__){
+                    func.__class__ = name+"."+getFunctionName(func);
+                }
+            }
+            else if(type=="object"&&!(value instanceof Array)){
+                setModuleName(value,name+"."+key);
+            }
+        }
+    }
 
 
 }
