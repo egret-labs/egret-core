7d64a9929280b08caa5cc2c4fb38e6b36aea6ec4
DOM <dom@domlib.com>
2014-05-26 21:34:18 +0800

    增加SpriteSheet解析器

---
+++ tools/templates/game/src/resource/analyzer/SheetAnalyzer.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) Egret-Labs.org. Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/// <reference path="BinAnalyzer.ts"/>
+
+module ns_egret {
+    /**
+     * SpriteSheet解析器
+     */
+    export class SheetAnalyzer extends BinAnalyzer{
+
+        public constructor(){
+            super();
+        }
+
+        private configMap:any = {};
+        /**
+         * 解析并缓存加载成功的数据
+         */
+        public analyzeData(resItem:ResourceItem,data:any):void{
+            var name:string = resItem.name;
+            if(this.fileDic[name]||!data){
+                return;
+            }
+            var config:any;
+            try{
+                var str:string = <string> data;
+                config = JSON.parse(str);
+            }
+            catch (e){
+            }
+            if(!config){
+                return;
+            }
+            this.configMap[name] - config;
+            config.bitmap
+        }
+    }
+}
