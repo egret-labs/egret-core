9087c2baffd24eb6065918eac2cc0b00a2a44f46
WanderWang <wander.wang@ismole.com>
2014-06-17 16:36:18 +0800

    优化hitTest性能
    优化hitTest性能

--- src/egret/display/DisplayObject.ts
+++ src/egret/display/DisplayObject.ts
@@ -635,7 +635,7 @@ module egret {
             if (!this.visible || (!ignoreTouchEnabled && !this._touchEnabled)) {
                 return null;
             }
-            var bound:Rectangle = this.getBounds(Rectangle.identity);
+            var bound:Rectangle = this._getSize(Rectangle.identity);
             if (0 < x && x < bound.width && 0 < y && y < bound.height) {
                 if (this.mask || this._scrollRect) {
                     if (this._scrollRect
