4c0e3e8ea72b164aaeda54d909482eb02d796afd
huanghaiying <friendhhy@163.com>
2014-07-03 13:27:29 +0800

    增加 兼容合图
    增加 兼容合图

--- src/egret/text/BitmapTextSpriteSheet.ts
+++ src/egret/text/BitmapTextSpriteSheet.ts
@@ -30,54 +30,59 @@
 
 module egret {
 
-    export class BitmapTextSpriteSheet extends SpriteSheet{
+    export class BitmapTextSpriteSheet extends SpriteSheet {
 
-        public constructor(bitmapData:any,fntText:string){
+        private _startX:number;
+        private _startY:number;
+
+        public constructor(bitmapData:any, fntText:string, startX:number = 0, startY:number = 0) {
             super(bitmapData);
             this.charList = this.parseConfig(fntText);
+            this._startX = startX;
+            this._startY = startY;
         }
 
         private charList:any;
 
-        public getTexture(name:string):Texture{
-            var texture:Texture =  this._textureMap[name];
-            if(!texture){
+        public getTexture(name:string):Texture {
+            var texture:Texture = this._textureMap[name];
+            if (!texture) {
                 var c:any = this.charList[name];
-                if(!c){
+                if (!c) {
                     return null;
                 }
-                texture = this.createTexture(name,c.x,c.y,c.width,c.height,c.offsetX,c.offsetY);
+                texture = this.createTexture(name, c.x + this._startX, c.y + this._startY, c.width, c.height, c.offsetX, c.offsetY);
                 this._textureMap[name] = texture;
             }
             return texture;
         }
 
-        private parseConfig(fntText:string):any{
+        private parseConfig(fntText:string):any {
             fntText = fntText.split("\r\n").join("\n");
             var lines:Array<string> = fntText.split("\n");
-            var charsCount:number = this.getConfigByKey(lines[3],"count");
+            var charsCount:number = this.getConfigByKey(lines[3], "count");
 
             var chars:any = {};
-            for (var i:number = 4 ; i < 4 + charsCount ; i++){
+            for (var i:number = 4; i < 4 + charsCount; i++) {
                 var charText:string = lines[i];
-                var letter:string = String.fromCharCode(this.getConfigByKey(charText,"id"));
+                var letter:string = String.fromCharCode(this.getConfigByKey(charText, "id"));
                 var c = {};
                 chars[letter] = c;
-                c["x"] = this.getConfigByKey(charText,"x");
-                c["y"] = this.getConfigByKey(charText,"y");
-                c["width"] = this.getConfigByKey(charText,"width");
-                c["height"] = this.getConfigByKey(charText,"height");
-                c["offsetX"] = this.getConfigByKey(charText,"xoffset");
-                c["offsetY"] = this.getConfigByKey(charText,"yoffset");
+                c["x"] = this.getConfigByKey(charText, "x");
+                c["y"] = this.getConfigByKey(charText, "y");
+                c["width"] = this.getConfigByKey(charText, "width");
+                c["height"] = this.getConfigByKey(charText, "height");
+                c["offsetX"] = this.getConfigByKey(charText, "xoffset");
+                c["offsetY"] = this.getConfigByKey(charText, "yoffset");
             }
             return chars;
         }
 
-        private getConfigByKey(configText:string,key:string):number{
+        private getConfigByKey(configText:string, key:string):number {
             var itemConfigTextList = configText.split(" ");
-            for (var i = 0 , length = itemConfigTextList.length ; i < length ; i++){
+            for (var i = 0 , length = itemConfigTextList.length; i < length; i++) {
                 var itemConfigText = itemConfigTextList[i];
-                if (key == itemConfigText.substring(0,key.length)){
+                if (key == itemConfigText.substring(0, key.length)) {
                     var value:string = itemConfigText.substring(key.length + 1);
                     return parseInt(value);
                 }
