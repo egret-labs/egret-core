0b0c63ed910a7e772d75e7c725e7a38065f77329
WanderWang <wander.wang@ismole.com>
2014-07-11 11:33:13 +0800

    项目模板的LoadingUI文字居中

--- tools/templates/game/src/LoadingUI.ts
+++ tools/templates/game/src/LoadingUI.ts
@@ -36,7 +36,6 @@ class LoadingUI extends egret.Sprite{
     private createView():void {
         this.textField = new egret.TextField();
         this.addChild(this.textField);
-        this.textField.x = 120;
         this.textField.y = 300;
         this.textField.width = 480;
         this.textField.height = 100;
