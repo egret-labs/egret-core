201e351ba5db00481694c1496484561bac12811a
himuil <lcj871126@126.com>
2014-08-15 10:53:44 +0800

    publish优化

--- tools/lib/core/locale/zh-CN.js
+++ tools/lib/core/locale/zh-CN.js
@@ -24,7 +24,6 @@ exports.error_code = {
     1603:"缺少create_app.json。\n请从以下地址下载最新手机平台支持库\nAndroid: http://www.egret-labs.org/download/egret-android-packager-download.html, \n\tiOS: http://www.egret-labs.org/download/egret-ios-packager-download.html",
     1604:"执行egret build命令失败",
     1605:"移动平台项目目录不能与html5项目目录为同一目录，请修改移动平台项目目录。",
-    1701:"缺少版本参数，请执行 {color_green}egret publish [project_name] --version [version]{color_normal} 发布项目",
     9999:"未知错误:{0}",
     8001:"请输入命令行选项{color_green} {0} {color_normal}的参数,并确认是以下值之一: {color_green}[{1}]{color_normal}",
     8002:"{color_red}请选对正确的egret项目目录！{color_normal}\n\tEgret_Project\t\t{color_gray}//项目目录{color_normal}\n\t\t--launcher\t{color_gray}//启动目录{color_normal}\n\t\t--src\t\t{color_gray}//源代码目录{color_normal}",
