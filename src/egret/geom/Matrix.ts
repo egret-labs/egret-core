18f0038a413e1da4bdb242d2cc42ef5ade2feba2
WanderWang <wander.wang@ismole.com>
2014-09-09 23:17:34 +0800

    优化 stage._updateTransform阶段性能
    优化 stage._updateTransform阶段性能

--- src/egret/geom/Matrix.ts
+++ src/egret/geom/Matrix.ts
@@ -105,11 +105,12 @@ module egret {
             var b1 = this.b;
             var c1 = this.c;
             var d1 = this.d;
-
-            this.a = a * a1 + b * c1;
-            this.b = a * b1 + b * d1;
-            this.c = c * a1 + d * c1;
-            this.d = c * b1 + d * d1;
+            if (a != 1 || b != 0 || c != 0 || d != 1) {
+                this.a = a * a1 + b * c1;
+                this.b = a * b1 + b * d1;
+                this.c = c * a1 + d * c1;
+                this.d = c * b1 + d * d1;
+            }
             this.tx = tx * a1 + ty * c1 + this.tx;
             this.ty = tx * b1 + ty * d1 + this.ty;
             return this;
