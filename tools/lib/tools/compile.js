920ec330bd7119431f8e538a678ecddbf0222592
WanderWang <wander.wang@ismole.com>
2014-10-09 18:17:36 +0800

    解决创建项目包括空格时，创建失败的bug

--- tools/lib/tools/compile.js
+++ tools/lib/tools/compile.js
@@ -378,7 +378,9 @@ function compileModule(callback, module, projectDir,outputPrefix) {
             var dependencyModule = getModuleConfig(dependencyList[i], projectDir);
             var dependencyModuleOutput = dependencyModule.output ? dependencyModule.output : dependencyModule.name;
             dependencyModuleOutput = path.join(projectDir, "libs", dependencyModuleOutput);
-            tsList.push(path.join(dependencyModuleOutput, dependencyModule.name + ".d.ts"));
+
+            var dtsFile = "\"" + path.join(dependencyModuleOutput, dependencyModule.name + ".d.ts") + "\"";
+            tsList.push(dtsFile);
         }
     }
 
