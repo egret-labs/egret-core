4797eb9bac988056f10ba40f95d905e66ccec9a6
himuil <lcj871126@126.com>
2014-12-25 15:33:18 +0800

    浏览器兼容

--- src/egret/context/renderer/HTML5CanvasRenderer.ts
+++ src/egret/context/renderer/HTML5CanvasRenderer.ts
@@ -105,7 +105,7 @@ module egret {
             return canvas;
         }
 
-        private onResize():void{
+        private onResize():void {
             //设置canvas宽高
             if (this.canvas) {
                 var container = document.getElementById(egret.StageDelegate.canvas_div_name);
@@ -141,7 +141,7 @@ module egret {
             this.canvasContext.clearRect(x, y, w, h);
         }
 
-        public drawImage(texture: Texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight, repeat=undefined) {
+        public drawImage(texture:Texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight, repeat = undefined) {
 
 //            if (DEBUG && DEBUG.DRAW_IMAGE) {
 //                DEBUG.checkDrawImage(texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
@@ -150,18 +150,18 @@ module egret {
             destX += this._transformTx;
             destY += this._transformTy;
             var beforeDraw = egret.getTimer();
-            if (repeat ===undefined) {
+            if (repeat === undefined) {
                 this._cacheCanvasContext.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
             }
             else {
                 this.drawRepeatImage(texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight, repeat);
             }
-            super.drawImage(texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight,repeat);
+            super.drawImage(texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight, repeat);
             this.renderCost += egret.getTimer() - beforeDraw;
         }
 
-        public drawRepeatImage(texture: Texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight, repeat) {
-            if(texture['pattern']===undefined){
+        public drawRepeatImage(texture:Texture, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight, repeat) {
+            if (texture['pattern'] === undefined) {
                 var texture_scale_factor = egret.MainContext.instance.rendererContext.texture_scale_factor;
                 var image = texture._bitmapData;
                 var tempImage:HTMLElement = image;
@@ -307,10 +307,24 @@ module egret {
             this._cacheCanvasContext.restore();
             this._cacheCanvasContext.setTransform(1, 0, 0, 1, 0, 0);
 
+            var canvasWidth = this._cacheCanvas.width;
+            var canvasHeight = this._cacheCanvas.height;
             var list = RenderFilter.getInstance().getDrawAreaList();
             for (var i:number = 0 , l:number = list.length; i < l; i++) {
                 var area:Rectangle = list[i];
-                this.canvasContext.drawImage(this._cacheCanvas, area.x, area.y, area.width, area.height, area.x, area.y, area.width, area.height);
+                var areaX = area.x;
+                var areaY = area.y;
+                var areaWidth = area.width;
+                var areaHeight = area.height;
+                if (areaX + areaWidth > canvasWidth) {
+                    areaWidth = canvasWidth - areaX;
+                }
+                if (areaY + areaHeight > canvasHeight) {
+                    areaHeight = canvasHeight - areaY;
+                }
+                if (areaWidth > 0 && areaHeight > 0) {
+                    this.canvasContext.drawImage(this._cacheCanvas, areaX, areaY, areaWidth, areaHeight, areaX, areaY, areaWidth, areaHeight);
+                }
             }
         }
     }
@@ -545,7 +559,7 @@ module egret_h5_graphics {
 
     export function _draw(renderContext:egret.RendererContext):void {
         var length = this.commandQueue.length;
-        if(length == 0) {
+        if (length == 0) {
             return;
         }
         this.renderContext = renderContext;
