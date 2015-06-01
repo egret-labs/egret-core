fcb003284115c3a5708c065412e448c237ab5ac8
mebius <mebiusashan@gmail.com>
2015-06-01 19:14:54 +0800

    将事件系统合并到Egret引擎体系中

--- src/extension/dragonbones/core/events/EventDispatcher.ts
+++ src/extension/dragonbones/core/events/EventDispatcher.ts
@@ -34,102 +34,11 @@ module dragonBones {
      * @classdesc
      * 事件派发者
      */
-    export class EventDispatcher {
-        private _listenersMap:Object;
+    export class EventDispatcher extends egret.EventDispatcher {
 
-        constructor() {
+        public constructor(target:egret.IEventDispatcher = null) {
+            super(target);
         }
 
-        /**
-         *是否有指定类型的事件侦听函数
-         * @param type 事件类型
-         * @returns {boolean}
-         */
-        public hasEventListener(type:string):boolean {
-            if(this._listenersMap && this._listenersMap[type]) {
-                return true;
-            }
-            return false;
-        }
-
-        /**
-         *添加一个针对指定类型的事件侦听函数
-         * @param type 事件类型
-         * @param listener 事件侦听函数
-         */
-        public addEventListener(type:string, listener:Function):void {
-            if (type && listener) {
-                if (!this._listenersMap) {
-                    this._listenersMap = {};
-                }
-                var listeners:Array<Function> = this._listenersMap[type];
-                if (listeners) {
-                    this.removeEventListener(type, listener);
-                }
-                if (listeners) {
-                    listeners.push(listener);
-                }
-                else {
-                    this._listenersMap[type] = [listener];
-                }
-            }
-        }
-
-        /**
-         *移除一个指定类型的事件侦听函数
-         * @param type 事件类型
-         * @param listener 需要移除的事件侦听函数
-         */
-        public removeEventListener(type:string, listener:Function):void {
-            if (!this._listenersMap || !type || !listener) {
-                return;
-            }
-            var listeners:Array<Function> = this._listenersMap[type];
-            if (listeners) {
-                var length:number = listeners.length;
-                for (var i:number = 0; i < length; i++) {
-                    if (listeners[i] == listener) {
-                        if (length == 1) {
-                            listeners.length = 0;
-                            delete this._listenersMap[type];
-                        }
-                        else {
-                            listeners.splice(i, 1);
-                        }
-                    }
-                }
-            }
-        }
-
-        /**
-         *移除指定类型的所有的事件侦听函数
-         * @param type 事件类型
-         */
-        public removeAllEventListeners(type:string):void {
-            if (type) {
-                delete this._listenersMap[type];
-            }
-            else {
-                this._listenersMap = null;
-            }
-        }
-
-        /**
-         * 派发事件
-         * @param event 被派发的事件
-         */
-        public dispatchEvent(event:Event):void {
-            if (event) {
-                var listeners:Array<Function> = this._listenersMap[event.type];
-                if (listeners) {
-                    event.target = this;
-                    var listenersCopy:Array<Function> = listeners.concat();
-                    var length:number = listeners.length;
-                    for (var i:number = 0; i < length; i++) {
-                        listenersCopy[i](event);
-                    }
-                }
-            }
-        }
     }
 }
