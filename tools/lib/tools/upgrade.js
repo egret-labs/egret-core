af6a39d61fd232042f03777122ef6a71f9695ecc
himuil <lcj871126@126.com>
2015-06-18 15:15:55 +0800

    upgrade to 2.0.0

--- tools/lib/tools/upgrade.js
+++ tools/lib/tools/upgrade.js
@@ -33,7 +33,8 @@ var upgradeConfigArr = [
     //{"v" : "1.7.0", "func":upgradeTo_1_7_0},
     //{"v" : "1.7.1", "func":upgradeTo_1_7_1},
     {"v" : "1.7.2", "func":upgradeTo_1_7_2},
-    {"v" : "1.7.3", "func":upgradeTo_1_7_3}
+    {"v" : "1.7.3", "func":upgradeTo_1_7_3},
+    {"v" : "2.0.0", "func":upgradeTo_2_0_0}
 ];
 
 var currDir;
@@ -357,6 +358,14 @@ function upgradeTo_1_7_3(){
 
     projectConfig.init(currDir);
     projectConfig.data.egret_version = "1.7.3";
+    projectConfig.save();
+}
+
+function upgradeTo_2_0_0() {
+    globals.log(1704, "2.0.0");
+
+    projectConfig.init(currDir);
+    projectConfig.data.egret_version = "2.0.0";
     projectConfig.save();
 }
 
