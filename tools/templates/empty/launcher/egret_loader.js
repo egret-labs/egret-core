41d9eb001897b0779787f4d50393356c4265e043
yjtx <friendhhy@163.com>
2015-07-02 19:25:26 +0800

    fix bug when create project

--- tools/templates/empty/launcher/egret_loader.js
+++ tools/templates/empty/launcher/egret_loader.js
@@ -28,52 +28,15 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 egret_h5.startGame = function () {
-    var context = egret.MainContext.instance;
-    context.touchContext = new egret.HTML5TouchContext();
-    context.deviceContext = new egret.HTML5DeviceContext();
-    context.netContext = new egret.HTML5NetContext();
 
-    egret.StageDelegate.getInstance().setDesignSize(480, 800);
-    context.stage = new egret.Stage();
-    var scaleMode = egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.SHOW_ALL : egret.StageScaleMode.NO_SCALE;
-    context.stage.scaleMode = scaleMode;
+    egret.web.runLark();
 
-    //WebGL is a Egret's beta property. It's off by default.
-    //WebGL是egret的Beta特性，默认关闭
-    var rendererType = 0;
-    if (rendererType == 1) {// egret.WebGLUtils.checkCanUseWebGL()) {
-        console.log("Use WebGL mode");
-        context.rendererContext = new egret.WebGLRenderer();
-    }
-    else {
-        context.rendererContext = new egret.HTML5CanvasRenderer();
-    }
-
-    egret.MainContext.instance.rendererContext.texture_scale_factor = 1;
-    context.run();
-
-    var rootClass;
-    if (document_class) {
-        rootClass = egret.getDefinitionByName(document_class);
-    }
-    if (rootClass) {
-        var rootContainer = new rootClass();
-        if (rootContainer instanceof egret.DisplayObjectContainer) {
-            context.stage.addChild(rootContainer);
-        }
-        else {
-            throw new Error("Document Class must be the subclass to egret.DisplayObjectContainer!");
-        }
-    }
-    else {
-        throw new Error("Document Class is not found！");
-    }
 
     //处理屏幕大小改变
     //implement for screen size change
     var resizeTimer = null;
     var doResize = function () {
-        context.stage.changeSize();
+        egret.web.updateScreenSize();
         resizeTimer = null;
     };
     window.onresize = function () {
