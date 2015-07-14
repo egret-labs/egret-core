cb8296c415553479afa5ef6909ae927fcdc84f25
yjtx <friendhhy@163.com>
2015-07-14 09:54:53 +0800

    audio load instead of cloneNode

--- src/egret/context/audio/Html5Audio.ts
+++ src/egret/context/audio/Html5Audio.ts
@@ -50,12 +50,12 @@ module egret {
             this.removeListeners();
 
             if (Html5Capatibility._System_OS != SystemOSType.WPHONE) {
-                this._audio = this._audio.cloneNode();
+                //this._audio = this._audio.cloneNode();
+                this._audio.load();
             }
             this.paused = false;
             this._audio.autoplay = true;
             this._audio.volume = this._volume;
-            //this._audio.load();
 
             var self = this;
             var func = function (e) {
