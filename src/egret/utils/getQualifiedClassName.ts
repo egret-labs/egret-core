863f640bfb9fd88da5e2d5f198843a883b767384
yjtx <friendhhy@163.com>
2015-07-15 15:10:06 +0800

    modify method

--- src/egret/utils/getQualifiedClassName.ts
+++ src/egret/utils/getQualifiedClassName.ts
@@ -28,29 +28,38 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 module egret {
+
     /**
      * @language en_US
      * Return the fully qualified class name of an object
-     * @param value {any} The object for which a fully qualified class name is desired. Any TypeScript / JavaScript value may be passed to this method including all available TypeScript / JavaScript types, object instances, primitive types such as number, and class objects."
-	 * @returns {string}
+     * @param value The object for which a fully qualified class name is desired. Any JavaScript value may be passed to
+     * this method including all available JavaScript types, object instances, primitive types such as number, and class objects.
      * @example
+     * <pre>
      *  egret.getQualifiedClassName(egret.DisplayObject) //return "egret.DisplayObject"
+     * </pre>
      * @version Egret 2.0
      * @platform Web,Native
      */
     /**
      * @language zh_CN
-     * 返回一个对象的完全限定名
-     * @param value {any} 需要完全限定类名称的对象，可以将任何 TypeScript / JavaScript值传递给此方法，包括所有可用的TypeScript / JavaScript类型、对象实例、原始类型（如number）和类对象
-	 * @returns {string}
+     * 返回对象的完全限定类名。
+     * @param value 需要完全限定类名称的对象，可以将任何 JavaScript 值传递给此方法，包括所有可用的 JavaScript 类型、对象实例、原始类型
+     * （如number)和类对象
      * @example
+     * <pre>
      *  egret.getQualifiedClassName(egret.DisplayObject) //返回 "egret.DisplayObject"
+     * </pre>
      * @version Egret 2.0
      * @platform Web,Native
      */
     export function getQualifiedClassName(value:any):string {
-        var prototype: any = value.prototype ? value.prototype : Object.getPrototypeOf(value);
-        if(prototype.hasOwnProperty("__class__")){
+        var type = typeof value;
+        if (!value || type != "object") {
+            return type;
+        }
+        var prototype:any = value.prototype ? value.prototype : Object.getPrototypeOf(value);
+        if (prototype.hasOwnProperty("__class__")) {
             return prototype["__class__"];
         }
         var constructorString:string = prototype.constructor.toString();
@@ -64,7 +73,8 @@ module egret {
         return className;
     }
 
-    /** @language en_US
+    /**
+     * @language en_US
      * Return the parent fully qualified class name of an object
      * @param value {any} The object for which a parent class is desired. Any TypeScript / JavaScript value may be passed to this method including all available TypeScript / JavaScript types, object instances, primitive types such as number, and class objects.
      * @returns {Function}
