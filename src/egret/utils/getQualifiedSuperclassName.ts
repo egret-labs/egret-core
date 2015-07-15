769324086984daee4a25513dd5da7c717b37d0cd
yjtx <friendhhy@163.com>
2015-07-15 15:21:43 +0800

    fix bug

--- src/egret/utils/getQualifiedSuperclassName.ts
+++ src/egret/utils/getQualifiedSuperclassName.ts
@@ -29,49 +29,43 @@
 
 module egret {
 
-    /**
-     * @language en_US
-     * Return the fully qualified class name of an object
-     * @param value The object for which a fully qualified class name is desired. Any JavaScript value may be passed to
-     * this method including all available JavaScript types, object instances, primitive types such as number, and class objects.
-     * @returns A string containing the fully qualified class name.
+    /** @language en_US
+     * Returns the fully qualified class name of the base class of the object specified by the value parameter.
+     * @param value The object for which a parent class is desired. Any JavaScript value may be passed to this method including
+     * all available JavaScript types, object instances, primitive types such as number, and class objects.
+     * @returns  A fully qualified base class name, or null if none exists.
      * @example
      * <pre>
-     *  egret.getQualifiedClassName(egret.DisplayObject) //return "egret.DisplayObject"
+     *  egret.getQualifiedSuperclassName(egret.Bitmap) //return "egret.DisplayObject"
      * </pre>
      * @version Egret 2.0
      * @platform Web,Native
      */
     /**
      * @language zh_CN
-     * 返回对象的完全限定类名。
-     * @param value 需要完全限定类名称的对象，可以将任何 JavaScript 值传递给此方法，包括所有可用的 JavaScript 类型、对象实例、原始类型
-     * （如number)和类对象
-     * @returns 包含完全限定类名称的字符串。
+     * 返回 value 参数指定的对象的基类的完全限定类名。
+     * @param value 需要取得父类的对象，可以将任何 JavaScript 值传递给此方法，包括所有可用的 JavaScript 类型、对象实例、原始类型（如number）和类对象
+     * @returns 完全限定的基类名称，或 null（如果不存在基类名称）。
      * @example
      * <pre>
-     *  egret.getQualifiedClassName(egret.DisplayObject) //返回 "egret.DisplayObject"
+     *  egret.getQualifiedSuperclassName(egret.Sprite) //返回 "egret.DisplayObject"
      * </pre>
      * @version Egret 2.0
      * @platform Web,Native
      */
-    export function getQualifiedClassName(value:any):string {
-        var type = typeof value;
-        if (!value || (type != "object"&&!value.prototype)) {
-            return type;
+    export function getQualifiedSuperclassName(value:any):string {
+        if (!value || typeof value != "object") {
+            return null;
         }
         var prototype:any = value.prototype ? value.prototype : Object.getPrototypeOf(value);
-        if (prototype.hasOwnProperty("__class__")) {
-            return prototype["__class__"];
+        var superProto = Object.getPrototypeOf(prototype);
+        if (!superProto) {
+            return null;
         }
-        var constructorString:string = prototype.constructor.toString();
-        var index:number = constructorString.indexOf("(");
-        var className:string = constructorString.substring(9, index);
-        Object.defineProperty(prototype, "__class__", {
-            value: className,
-            enumerable: false,
-            writable: true
-        });
-        return className;
+        var superClass = getQualifiedClassName(superProto.constructor);
+        if (!superClass) {
+            return null;
+        }
+        return superClass;
     }
 }
