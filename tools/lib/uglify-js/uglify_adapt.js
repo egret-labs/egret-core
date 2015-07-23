4f4e6f630bcdca874215283571f6d7584b28c0ee
yjtx <friendhhy@163.com>
2015-07-23 18:06:05 +0800

    modify uglify

--- tools/lib/uglify-js/uglify_adapt.js
+++ tools/lib/uglify-js/uglify_adapt.js
@@ -9,18 +9,9 @@ exports.compilerSingleFile = function (fileList, outputFile, tempFile, callback)
         RELEASE: true
     };
 
-    fileList = fileList.filter(function (item) {
-        if (item.indexOf("")) {
-
-        }
-    });
-
     var result = uglifyJs.minify(fileList, { compress: { global_defs: defines }, output: { beautify: false } });
 
-    //var result = uglifyJs.minify(fileList);
     file.save(outputFile, result.code);
 
-    setTimeout(function () {
-        callback();
-    }, 100);
+    callback();
 };
