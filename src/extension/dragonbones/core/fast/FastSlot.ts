378a0a4a91d0644c657a68168985631a07d1e124
sucresk <sucresk@gmail.com>
2015-07-31 11:49:35 +0800

    fix instanceof 不能判断接口的bug

--- src/extension/dragonbones/core/fast/FastSlot.ts
+++ src/extension/dragonbones/core/fast/FastSlot.ts
@@ -292,7 +292,8 @@ module dragonBones {
 		 * @member {FastArmature} dragonBones.Slot#childArmature
 		 */
 		public get childArmature():any{
-			return this._displayList[this._currentDisplayIndex] instanceof IArmature ? this._displayList[this._currentDisplayIndex] : null;
+			return (this._displayList[this._currentDisplayIndex] instanceof Armature 
+				    || this._displayList[this._currentDisplayIndex] instanceof FastArmature) ? this._displayList[this._currentDisplayIndex] : null;
 		}
 		
 		public set childArmature(value:any)
