8aa55e43619071a80dd11e72ef1d627f145e0251
yjtx <friendhhy@163.com>
2015-08-07 19:41:18 +0800

    change notes

--- src/egret/context/WebTexture.ts
+++ src/egret/context/WebTexture.ts
@@ -106,7 +106,7 @@ module egret.web {
         var aLink = document.createElement('a');
         var blob = new Blob([content]);
         var evt = document.createEvent("HTMLEvents");
-        evt.initEvent("click", false, false);//initEvent 不加后两个参数在FF下会报错, 感谢 Barret Lee 的反馈
+        evt.initEvent("click", false, false);//initEvent 不加后两个参数在FF下会报错
         aLink['download'] = fileName;
         var winURL = window["URL"] || window["webkitURL"];
         aLink.href = winURL.createObjectURL(blob);
