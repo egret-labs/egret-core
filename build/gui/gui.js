fcac69f3a11f5d8792c9f7f097f7245fb7e47651
yjtx <friendhhy@163.com>
2015-09-14 18:22:27 +0800

    build

--- build/gui/gui.js
+++ build/gui/gui.js
@@ -16814,7 +16814,236 @@ var egret;
 //  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //////////////////////////////////////////////////////////////////////////////////////
+var egret;
+(function (egret) {
+    var gui;
+    (function (gui) {
+        /**
+         * @class egret.gui.TreeItemRenderer
+         * @classdesc
+         * Tree组件的项呈示器基类
+         * @extends egret.gui.ItemRenderer
+         * @implements egret.gui.ITreeItemRenderer
+         */
+        var TreeItemRenderer = (function (_super) {
+            __extends(TreeItemRenderer, _super);
+            /**
+             * 构造函数
+             * @method egret.gui.TreeItemRenderer#constructor
+             */
+            function TreeItemRenderer() {
+                _super.call(this);
+                /**
+                 * [SkinPart]图标显示对象
+                 * @member egret.gui.TreeItemRenderer#iconDisplay
+                 */
+                this.iconDisplay = null;
+                /**
+                 * [SkinPart]子节点开启按钮
+                 * @member egret.gui.TreeItemRenderer#disclosureButton
+                 */
+                this.disclosureButton = null;
+                /**
+                 * [SkinPart]用于调整缩进值的容器对象。
+                 * @member egret.gui.TreeItemRenderer#contentGroup
+                 */
+                this.contentGroup = null;
+                /**
+                 *
+                 * @type {number}
+                 * @private
+                 */
+                this._indentation = 17;
+                /**
+                 *
+                 * @type {null}
+                 * @private
+                 */
+                this._iconSkinName = null;
+                /**
+                 *
+                 * @type {number}
+                 * @private
+                 */
+                this._depth = 0;
+                /**
+                 *
+                 * @type {boolean}
+                 * @private
+                 */
+                this._hasChildren = false;
+                /**
+                 *
+                 * @type {boolean}
+                 * @private
+                 */
+                this._isOpen = false;
+                this.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onItemMouseDown, this, false, 1000);
+            }
+            var d = __define,c=TreeItemRenderer;p=c.prototype;
+            p.onItemMouseDown = function (event) {
+                if (event.target == this.disclosureButton) {
+                    event.stopImmediatePropagation();
+                }
+            };
+            d(p, "indentation"
+                /**
+                 * 子节点相对父节点的缩进值，以像素为单位。默认17。
+                 * @member egret.gui.TreeItemRenderer#indentation
+                 */
+                ,function () {
+                    return this._indentation;
+                }
+                ,function (value) {
+                    this._indentation = value;
+                }
+            );
+            d(p, "iconSkinName"
+                /**
+                 * @member egret.gui.TreeItemRenderer#iconSkinName
+                 */
+                ,function () {
+                    return this._iconSkinName;
+                }
+                ,function (value) {
+                    if (this._iconSkinName == value)
+                        return;
+                    this._iconSkinName = value;
+                    if (this.iconDisplay) {
+                        this.iconDisplay.source = this._iconSkinName;
+                    }
+                }
+            );
+            d(p, "depth"
+                /**
+                 * @member egret.gui.TreeItemRenderer#depth
+                 */
+                ,function () {
+                    return this._depth;
+                }
+                ,function (value) {
+                    if (value == this._depth)
+                        return;
+                    this._depth = value;
+                    if (this.contentGroup) {
+                        this.contentGroup.x = this._depth * this._indentation;
+                    }
+                }
+            );
+            d(p, "hasChildren"
+                /**
+                 * @member egret.gui.TreeItemRenderer#hasChildren
+                 */
+                ,function () {
+                    return this._hasChildren;
+                }
+                ,function (value) {
+                    if (this._hasChildren == value)
+                        return;
+                    this._hasChildren = value;
+                    if (this.disclosureButton) {
+                        this.disclosureButton.visible = this._hasChildren;
+                    }
+                }
+            );
+            d(p, "opened"
+                /**
+                 * @member egret.gui.TreeItemRenderer#opened
+                 */
+                ,function () {
+                    return this._isOpen;
+                }
+                ,function (value) {
+                    if (this._isOpen == value)
+                        return;
+                    this._isOpen = value;
+                    if (this.disclosureButton) {
+                        this.disclosureButton.selected = this._isOpen;
+                    }
+                }
+            );
+            /**
+             * 添加外观部件时调用
+             * @method egret.gui.TreeItemRenderer#partAdded
+             * @param partName {string}
+             * @param instance {any}
+             */
+            p.partAdded = function (partName, instance) {
+                _super.prototype.partAdded.call(this, partName, instance);
+                if (instance == this.iconDisplay) {
+                    this.iconDisplay.source = this._iconSkinName;
+                }
+                else if (instance == this.disclosureButton) {
+                    this.disclosureButton.visible = this._hasChildren;
+                    this.disclosureButton.selected = this._isOpen;
+                    this.disclosureButton._autoSelected = false;
+                    this.disclosureButton.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.disclosureButton_mouseDownHandler, this);
+                }
+                else if (instance == this.contentGroup) {
+                    this.contentGroup.x = this._depth * this._indentation;
+                }
+            };
+            /**
+             * 删除外观部件的实例时调用
+             * @method egret.gui.TreeItemRenderer#partRemoved
+             * @param partName {string}
+             * @param instance {any}
+             */
+            p.partRemoved = function (partName, instance) {
+                _super.prototype.partRemoved.call(this, partName, instance);
+                if (instance == this.iconDisplay) {
+                    this.iconDisplay.source = null;
+                }
+                else if (instance == this.disclosureButton) {
+                    this.disclosureButton.removeEventListener(egret.TouchEvent.TOUCH_BEGIN, this.disclosureButton_mouseDownHandler, this);
+                    this.disclosureButton._autoSelected = true;
+                    this.disclosureButton.visible = true;
+                }
+            };
+            /**
+             * 鼠标在disclosureButton上按下
+             * @method egret.gui.TreeItemRenderer#disclosureButton_mouseDownHandler
+             * @param event {TouchEvent}
+             */
+            p.disclosureButton_mouseDownHandler = function (event) {
+                gui.TreeEvent.dispatchTreeEvent(this, gui.TreeEvent.ITEM_OPENING, this.itemIndex, this.data, this, !this._isOpen, false, true);
+            };
+            return TreeItemRenderer;
+        })(gui.ItemRenderer);
+        gui.TreeItemRenderer = TreeItemRenderer;
+        egret.registerClass(TreeItemRenderer,"egret.gui.TreeItemRenderer",["egret.gui.ITreeItemRenderer","egret.gui.IItemRenderer","egret.gui.ILayoutElement","egret.IEventDispatcher"]);
+    })(gui = egret.gui || (egret.gui = {}));
+})(egret || (egret = {}));
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
 /// <reference path="../core/ClassFactory.ts" />
+/// <reference path="./supportClasses/TreeItemRenderer.ts" />
 var egret;
 (function (egret) {
     var gui;
@@ -19631,234 +19860,6 @@ var egret;
         })();
         gui.Theme = Theme;
         egret.registerClass(Theme,"egret.gui.Theme");
-    })(gui = egret.gui || (egret.gui = {}));
-})(egret || (egret = {}));
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-2015, Egret Technology Inc.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
-var egret;
-(function (egret) {
-    var gui;
-    (function (gui) {
-        /**
-         * @class egret.gui.TreeItemRenderer
-         * @classdesc
-         * Tree组件的项呈示器基类
-         * @extends egret.gui.ItemRenderer
-         * @implements egret.gui.ITreeItemRenderer
-         */
-        var TreeItemRenderer = (function (_super) {
-            __extends(TreeItemRenderer, _super);
-            /**
-             * 构造函数
-             * @method egret.gui.TreeItemRenderer#constructor
-             */
-            function TreeItemRenderer() {
-                _super.call(this);
-                /**
-                 * [SkinPart]图标显示对象
-                 * @member egret.gui.TreeItemRenderer#iconDisplay
-                 */
-                this.iconDisplay = null;
-                /**
-                 * [SkinPart]子节点开启按钮
-                 * @member egret.gui.TreeItemRenderer#disclosureButton
-                 */
-                this.disclosureButton = null;
-                /**
-                 * [SkinPart]用于调整缩进值的容器对象。
-                 * @member egret.gui.TreeItemRenderer#contentGroup
-                 */
-                this.contentGroup = null;
-                /**
-                 *
-                 * @type {number}
-                 * @private
-                 */
-                this._indentation = 17;
-                /**
-                 *
-                 * @type {null}
-                 * @private
-                 */
-                this._iconSkinName = null;
-                /**
-                 *
-                 * @type {number}
-                 * @private
-                 */
-                this._depth = 0;
-                /**
-                 *
-                 * @type {boolean}
-                 * @private
-                 */
-                this._hasChildren = false;
-                /**
-                 *
-                 * @type {boolean}
-                 * @private
-                 */
-                this._isOpen = false;
-                this.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onItemMouseDown, this, false, 1000);
-            }
-            var d = __define,c=TreeItemRenderer;p=c.prototype;
-            p.onItemMouseDown = function (event) {
-                if (event.target == this.disclosureButton) {
-                    event.stopImmediatePropagation();
-                }
-            };
-            d(p, "indentation"
-                /**
-                 * 子节点相对父节点的缩进值，以像素为单位。默认17。
-                 * @member egret.gui.TreeItemRenderer#indentation
-                 */
-                ,function () {
-                    return this._indentation;
-                }
-                ,function (value) {
-                    this._indentation = value;
-                }
-            );
-            d(p, "iconSkinName"
-                /**
-                 * @member egret.gui.TreeItemRenderer#iconSkinName
-                 */
-                ,function () {
-                    return this._iconSkinName;
-                }
-                ,function (value) {
-                    if (this._iconSkinName == value)
-                        return;
-                    this._iconSkinName = value;
-                    if (this.iconDisplay) {
-                        this.iconDisplay.source = this._iconSkinName;
-                    }
-                }
-            );
-            d(p, "depth"
-                /**
-                 * @member egret.gui.TreeItemRenderer#depth
-                 */
-                ,function () {
-                    return this._depth;
-                }
-                ,function (value) {
-                    if (value == this._depth)
-                        return;
-                    this._depth = value;
-                    if (this.contentGroup) {
-                        this.contentGroup.x = this._depth * this._indentation;
-                    }
-                }
-            );
-            d(p, "hasChildren"
-                /**
-                 * @member egret.gui.TreeItemRenderer#hasChildren
-                 */
-                ,function () {
-                    return this._hasChildren;
-                }
-                ,function (value) {
-                    if (this._hasChildren == value)
-                        return;
-                    this._hasChildren = value;
-                    if (this.disclosureButton) {
-                        this.disclosureButton.visible = this._hasChildren;
-                    }
-                }
-            );
-            d(p, "opened"
-                /**
-                 * @member egret.gui.TreeItemRenderer#opened
-                 */
-                ,function () {
-                    return this._isOpen;
-                }
-                ,function (value) {
-                    if (this._isOpen == value)
-                        return;
-                    this._isOpen = value;
-                    if (this.disclosureButton) {
-                        this.disclosureButton.selected = this._isOpen;
-                    }
-                }
-            );
-            /**
-             * 添加外观部件时调用
-             * @method egret.gui.TreeItemRenderer#partAdded
-             * @param partName {string}
-             * @param instance {any}
-             */
-            p.partAdded = function (partName, instance) {
-                _super.prototype.partAdded.call(this, partName, instance);
-                if (instance == this.iconDisplay) {
-                    this.iconDisplay.source = this._iconSkinName;
-                }
-                else if (instance == this.disclosureButton) {
-                    this.disclosureButton.visible = this._hasChildren;
-                    this.disclosureButton.selected = this._isOpen;
-                    this.disclosureButton._autoSelected = false;
-                    this.disclosureButton.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.disclosureButton_mouseDownHandler, this);
-                }
-                else if (instance == this.contentGroup) {
-                    this.contentGroup.x = this._depth * this._indentation;
-                }
-            };
-            /**
-             * 删除外观部件的实例时调用
-             * @method egret.gui.TreeItemRenderer#partRemoved
-             * @param partName {string}
-             * @param instance {any}
-             */
-            p.partRemoved = function (partName, instance) {
-                _super.prototype.partRemoved.call(this, partName, instance);
-                if (instance == this.iconDisplay) {
-                    this.iconDisplay.source = null;
-                }
-                else if (instance == this.disclosureButton) {
-                    this.disclosureButton.removeEventListener(egret.TouchEvent.TOUCH_BEGIN, this.disclosureButton_mouseDownHandler, this);
-                    this.disclosureButton._autoSelected = true;
-                    this.disclosureButton.visible = true;
-                }
-            };
-            /**
-             * 鼠标在disclosureButton上按下
-             * @method egret.gui.TreeItemRenderer#disclosureButton_mouseDownHandler
-             * @param event {TouchEvent}
-             */
-            p.disclosureButton_mouseDownHandler = function (event) {
-                gui.TreeEvent.dispatchTreeEvent(this, gui.TreeEvent.ITEM_OPENING, this.itemIndex, this.data, this, !this._isOpen, false, true);
-            };
-            return TreeItemRenderer;
-        })(gui.ItemRenderer);
-        gui.TreeItemRenderer = TreeItemRenderer;
-        egret.registerClass(TreeItemRenderer,"egret.gui.TreeItemRenderer",["egret.gui.ITreeItemRenderer","egret.gui.IItemRenderer","egret.gui.ILayoutElement","egret.IEventDispatcher"]);
     })(gui = egret.gui || (egret.gui = {}));
 })(egret || (egret = {}));
 //////////////////////////////////////////////////////////////////////////////////////
