071cde48b716854970bfe1e66da70097e8c269b5
yjtx <friendhhy@163.com>
2015-09-23 15:13:30 +0800

    build

--- build/res/res.js
+++ build/res/res.js
@@ -2130,7 +2130,7 @@ var RES;
                     return url;
                 }
                 if (this._versionInfo && this._versionInfo[url]) {
-                    return "resource/" + this._versionInfo[url]["v"].substring(0, 2) + "/" + this._versionInfo[url]["v"] + "_" + this._versionInfo[url]["s"];
+                    return "resource/" + this._versionInfo[url]["v"].substring(0, 2) + "/" + this._versionInfo[url]["v"] + "_" + this._versionInfo[url]["s"] + "." + url.substring(url.lastIndexOf(".") + 1);
                 }
                 else {
                     return url;
@@ -2251,7 +2251,7 @@ var RES;
                     return url;
                 }
                 if (this._versionInfo && this._versionInfo[url]) {
-                    return "resource/" + this._versionInfo[url]["v"].substring(0, 2) + "/" + this._versionInfo[url]["v"] + "_" + this._versionInfo[url]["s"];
+                    return "resource/" + this._versionInfo[url]["v"].substring(0, 2) + "/" + this._versionInfo[url]["v"] + "_" + this._versionInfo[url]["s"] + "." + url.substring(url.lastIndexOf(".") + 1);
                 }
                 else {
                     return url;
