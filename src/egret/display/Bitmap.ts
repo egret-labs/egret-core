9a9e7c684e19611c3960ebce200119a18f07d1ae
yjtx <friendhhy@163.com>
2015-09-29 10:44:28 +0800

    fix bug for bitmap repeat

--- src/egret/display/Bitmap.ts
+++ src/egret/display/Bitmap.ts
@@ -540,7 +540,7 @@ module egret {
                     tempCanvas = egret.sys.surfaceFactory.create(true);
                     tempCanvas.width = textureWidth;
                     tempCanvas.height = textureHeight;
-                    tempCanvas.renderContext.drawImage(tempImage, clipX, clipY, clipWidth, clipHeight, offsetX, offsetY, textureWidth, textureHeight);
+                    tempCanvas.renderContext.drawImage(tempImage, clipX, clipY, clipWidth, clipHeight, offsetX, offsetY, clipX * $TextureScaleFactor, clipY * $TextureScaleFactor);
                     tempImage = tempCanvas;
                 }
 
