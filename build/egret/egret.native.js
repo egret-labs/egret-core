ddf9258bddd2850e1c1171e5b125a406c614765b
himuil <lcj871126@126.com>
2015-09-30 11:48:01 +0800

    parse xml in native

--- build/egret/egret.native.js
+++ build/egret/egret.native.js
@@ -2796,6 +2796,154 @@ var egret;
 //////////////////////////////////////////////////////////////////////////////////////
 var egret;
 (function (egret) {
+    var web;
+    (function (web) {
+        /**
+         * @private
+         * XML节点基类
+         */
+        var XMLNode = (function () {
+            /**
+             * @private
+             */
+            function XMLNode(nodeType, parent) {
+                this.nodeType = nodeType;
+                this.parent = parent;
+            }
+            var d = __define,c=XMLNode;p=c.prototype;
+            return XMLNode;
+        })();
+        web.XMLNode = XMLNode;
+        egret.registerClass(XMLNode,"egret.web.XMLNode");
+        /**
+         * @private
+         * XML节点对象
+         */
+        var XML = (function (_super) {
+            __extends(XML, _super);
+            /**
+             * @private
+             */
+            function XML(localName, parent, prefix, namespace, name) {
+                _super.call(this, 1, parent);
+                /**
+                 * @private
+                 * 当前节点上的属性列表
+                 */
+                this.attributes = {};
+                /**
+                 * @private
+                 * 当前节点的子节点列表
+                 */
+                this.children = [];
+                this.localName = localName;
+                this.prefix = prefix;
+                this.namespace = namespace;
+                this.name = name;
+            }
+            var d = __define,c=XML;p=c.prototype;
+            return XML;
+        })(XMLNode);
+        web.XML = XML;
+        egret.registerClass(XML,"egret.web.XML");
+        /**
+         * @private
+         * XML文本节点
+         */
+        var XMLText = (function (_super) {
+            __extends(XMLText, _super);
+            /**
+             * @private
+             */
+            function XMLText(text, parent) {
+                _super.call(this, 3, parent);
+                this.text = text;
+            }
+            var d = __define,c=XMLText;p=c.prototype;
+            return XMLText;
+        })(XMLNode);
+        web.XMLText = XMLText;
+        egret.registerClass(XMLText,"egret.web.XMLText");
+        /**
+         * @private
+         * 解析字符串为XML对象
+         * @param text 要解析的字符串
+         */
+        function parse(text) {
+            var xmlDocStr = egret_native.xmlStr2JsonStr(text);
+            console.log(xmlDocStr);
+            var xmlDoc = JSON.parse(xmlDocStr);
+            return parseNode(xmlDoc, null);
+        }
+        /**
+         * @private
+         * 解析一个节点
+         */
+        function parseNode(node, parent) {
+            if (node.localName == "parsererror") {
+                throw new Error(node.textContent);
+            }
+            var xml = new XML(node.localName, parent, node.prefix, node.namespace, node.name);
+            var nodeAttributes = node.attributes;
+            var attributes = xml.attributes;
+            for (var key in nodeAttributes) {
+                attributes[key] = nodeAttributes[key];
+            }
+            var childNodes = node.children;
+            var length = childNodes.length;
+            var children = xml.children;
+            for (var i = 0; i < length; i++) {
+                var childNode = childNodes[i];
+                var nodeType = childNode.nodeType;
+                var childXML = null;
+                if (nodeType == 1) {
+                    childXML = parseNode(childNode, xml);
+                }
+                else if (nodeType == 3) {
+                    var text = childNode.text.trim();
+                    if (text) {
+                        childXML = new XMLText(text, xml);
+                    }
+                }
+                if (childXML) {
+                    children.push(childXML);
+                }
+            }
+            return xml;
+        }
+        egret.XML = { parse: parse };
+    })(web = egret.web || (egret.web = {}));
+})(egret || (egret = {}));
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+var egret;
+(function (egret) {
     var native;
     (function (native) {
         if (DEBUG) {
