d750a0a6e59ca54e78f9c07c1ec21b95c1fa0cc2
yjtx <friendhhy@163.com>
2015-10-12 11:49:17 +0800

    add minsize

--- src/egret/player/DisplayList.ts
+++ src/egret/player/DisplayList.ts
@@ -679,8 +679,8 @@ module egret.sys {
             var oldSurface = oldContext.surface;
             if (!this.sizeChanged) {
                 this.sizeChanged = true;
-                oldSurface.width = bounds.width * scaleX;
-                oldSurface.height = bounds.height * scaleY;
+                oldSurface.width = Math.max(bounds.width * scaleX, 257);
+                oldSurface.height = Math.max(bounds.height * scaleY, 257);
             }
             else {
                 var newContext = sys.sharedRenderContext;
@@ -688,12 +688,12 @@ module egret.sys {
                 sys.sharedRenderContext = oldContext;
                 this.renderContext = newContext;
                 this.surface = newSurface;
-                newSurface.width = bounds.width * scaleX;
-                newSurface.height = bounds.height * scaleY;
-                if (oldSurface.width !== 0 && oldSurface.height !== 0) {
+                newSurface.width = Math.max(bounds.width * scaleX, 257);
+                newSurface.height = Math.max(bounds.height * scaleY, 257);
+                //if (bounds.width !== 0 && bounds.height !== 0) {
                     newContext.setTransform(1, 0, 0, 1, 0, 0);
                     newContext.drawImage(oldSurface, (oldOffsetX - this.offsetX) * scaleX, (oldOffsetY - this.offsetY) * scaleY);
-                }
+                //}
                 if (Capabilities.runtimeType != RuntimeType.NATIVE) {
                     oldSurface.height = 1;
                     oldSurface.width = 1;
