42e7a55c087128ece550895c1e5e413605c923da
himuil <lcj871126@126.com>
2015-10-16 17:15:52 +0800

    modify note

--- src/egret/media/Video.ts
+++ src/egret/media/Video.ts
@@ -41,7 +41,7 @@ module egret {
      * @event egret.Event.ENDED Dispatch when the video playback ended
      * @event egret.IOErrorEvent.IO_ERROR when the video is failed to load
      * @version Egret 2.4
-     * @platform Web,Native
+     * @platform Web
      * @includeExample egret/media/Video.ts
      */
     /**
@@ -56,7 +56,7 @@ module egret {
      * @event egret.Event.ENDED 视频播放完成时抛出
      * @event egret.IOErrorEvent.IO_ERROR 视频加载失败时触发
      * @version Egret 2.4
-     * @platform Web,Native
+     * @platform Web
      * @includeExample egret/media/Video.ts
      */
     export interface Video extends DisplayObject {
@@ -66,14 +66,14 @@ module egret {
          * Initiates loading of an external video file from the specified URL.
          * @param url Audio file URL
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 启动从指定 URL 加载外部视频文件的过程。
          * @param url 需要加载的视频文件URL
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         load(url:string): void;
 
@@ -84,7 +84,7 @@ module egret {
          * @param loop Defines should play the video again when the video is ended. (default = false)
          * @param fullscreen Defines should play the video in fullscreen mode. (default = false)
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
@@ -93,7 +93,7 @@ module egret {
          * @param loop 是否需要循环播放，默认值是 false
          * @param fullscreen 是否需要全屏播放，默认值是 false
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         play(startTime?:number, loop?:boolean);
 
@@ -101,13 +101,13 @@ module egret {
          * @language en_US
          * Closes the stream, causing any download of data to cease
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 关闭该流，从而停止所有数据的下载。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         close(): void;
 
@@ -115,13 +115,13 @@ module egret {
          * @language en_US
          * The URL of the video you want to play.
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 想要播放的视频的URL
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         src: string;
 
@@ -129,13 +129,13 @@ module egret {
          * @language en_US
          * The URL of an image you want to display before the video is loaded or video cannot been draw on the canvas on some mobile device.
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 视频加载前，或者在不支持将 video 画在 canvas 的设备上，想要显示的视频截图地址。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         poster: string;
 
@@ -144,27 +144,27 @@ module egret {
          * Should play the video in fullscreen mode (default = true).
          * Currently only supports full-screen mobile terminal web.
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 是否全屏播放这个视频（默认值是 true）。
          * 目前移动端 web 只支持全屏。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         fullscreen: boolean;
         /**
          * @language en_US
          * The volume, ranging from 0 (silent) to 1 (full volume).
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 音量范围从 0（静音）至 1（最大音量）。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         volume: number;
 
@@ -173,13 +173,13 @@ module egret {
          * When the video is playing, the position property indicates
          * in seconds the current point that is being played in the video file.
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 当播放视频时，position 属性表示视频文件中当前播放的位置（以秒为单位）
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         position: number;
 
@@ -187,13 +187,13 @@ module egret {
          * @language en_US
          * Pause the video playing.
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          * 暂停播放。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         pause(): void;
 
@@ -203,14 +203,14 @@ module egret {
          * Note: On most mobile device, the video is playback in the full screen mode.
          * So you can just use the play() method instead of draw it on the Stage
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         /**
          * @language zh_CN
          *  获取视频的 bitmapData, 你可以将视频绘制到舞台上。
          * 注意： 在大多数移动设备中，视频是全屏播放的，所以你可以直接调用 play() 方法全屏播放视频，不用将它绘制在Stage中。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          */
         bitmapData: BitmapData;
 
@@ -218,14 +218,14 @@ module egret {
          * @language en_US
          * Whether current video is paused.
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          * @readOnly
          */
         /**
          * @language zh_CN
          * 当前视频是否在暂停状态。
          * @version Egret 2.4
-         * @platform Web,Native
+         * @platform Web
          * @readOnly
          */
         paused:boolean;
