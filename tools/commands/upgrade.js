32814a209ffd4999bd53c4f901c519158998c3ba
himuil <lcj871126@126.com>
2015-10-16 11:58:27 +0800

    upgrade to 2.5.2

--- tools/commands/upgrade.js
+++ tools/commands/upgrade.js
@@ -38,7 +38,8 @@ var UpgradeCommand = (function () {
             //{"v": "2.4.0"},
             //{"v": "2.4.1"},
             { "v": "2.5.0", "command": require("./upgrade/UpgradeCommand_2_4_3") },
-            { "v": "2.5.1", "command": require("./upgrade/UpgradeCommand_2_5_1") }
+            { "v": "2.5.1", "command": require("./upgrade/UpgradeCommand_2_5_1") },
+            { "v": "2.5.2"}
         ];
         //升级命令是一个异步命令 内含异步控制流程
         this.isAsync = true;
