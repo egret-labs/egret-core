16077ed028409fff59283a0309f201232e57b3cc
himuil <lcj871126@126.com>
2015-11-23 15:32:08 +0800

    fix bug for Graphics

--- src/egret/display/DisplayObject.ts
+++ src/egret/display/DisplayObject.ts
@@ -1955,7 +1955,7 @@ module egret {
          * 注意，不要在大量物体中使用精确碰撞像素检测，这回带来巨大的性能开销
          * @param x {number}  要测试的此对象的 x 坐标。
          * @param y {number}  要测试的此对象的 y 坐标。
-         * @param shapeFlag {boolean} 是检查对象 (true) 的实际像素，还是检查边框 (false) 的实际像素。暂未实现。
+         * @param shapeFlag {boolean} 是检查对象 (true) 的实际像素，还是检查边框 (false) 的实际像素。
          * @returns {boolean} 如果显示对象与指定的点重叠或相交，则为 true；否则为 false。
          * @version Egret 2.4
          * @platform Web,Native
