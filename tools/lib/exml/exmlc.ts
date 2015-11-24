474535306b4839d33dd67e8f311dbb0bbbfa0f2b
yanjiaqi <yanjiaqi@egret.com>
2015-11-24 14:48:46 +0800

    error 2005 fix to warn

--- tools/lib/exml/exmlc.ts
+++ tools/lib/exml/exmlc.ts
@@ -603,7 +603,7 @@ class EXMLCompiler{
                 }
                 var type:string = this.exmlConfig.getPropertyType(child.localName,className);
                 if(!type){
-                    this.exit(2005,this.exmlPath,child.localName,this.getPropertyStr(child));
+                    globals.warn(2005,this.exmlPath,child.localName,this.getPropertyStr(child));
                 }
                 if(!child.children||child.children.length==0){
                     globals.warn(2102,this.exmlPath,this.getPropertyStr(child));
@@ -759,7 +759,7 @@ class EXMLCompiler{
         var className:string = this.exmlConfig.getClassNameById(node.localName,node.namespace);
         var type:string = this.exmlConfig.getPropertyType(key,className);
         if(!type){
-            this.exit(2005,this.exmlPath,key,this.toXMLString(node));
+            globals.warn(2005,this.exmlPath,key,this.toXMLString(node));
         }
         if(type!="string"&&value.charAt(0)=="{"&&value.charAt(value.length-1)=="}"){
             value = value.substr(1,value.length-2);
