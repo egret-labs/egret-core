d431f6c45e568cb579bf4bb2c7db78c2da933c45
sucresk <sucresk@gmail.com>
2015-12-02 18:11:47 +0800

    4.3.1 fix buildArmature bug

--- src/extension/dragonbones/core/DragonBones.ts
+++ src/extension/dragonbones/core/DragonBones.ts
@@ -44,7 +44,7 @@ module dragonBones {
 		 */
         public static PARENT_COORDINATE_DATA_VERSION:string = "3.0";
 
-        public static VERSION:string = "4.3.0";
+        public static VERSION:string = "4.3.1";
 		
 		public constructor(){
 		}
