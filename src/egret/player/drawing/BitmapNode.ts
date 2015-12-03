870ba2ae0315cb9fae68a6ceb54222af1468a4f0
dom <dom@domlib.com>
2015-12-03 11:52:59 +0800

    add render nodes

---
+++ src/egret/player/drawing/BitmapNode.ts
@@ -0,0 +1,64 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+module egret.sys {
+
+    /**
+     * @private
+     * 位图渲染节点
+     */
+    export class BitmapNode extends RenderNode {
+
+        /**
+         * 要绘制的位图
+         */
+        public image:BitmapData = null;
+        /**
+         * 控制在缩放时是否对位图进行平滑处理。
+         */
+        public smoothing:boolean = true;
+        /**
+         * 绘制数据
+         */
+        $drawData:any[] = [];
+        /**
+         * 绘制一次位图
+         */
+        public draw(sourceX:number, sourceY:number, sourceW:number, sourceH:number,
+                         drawX:number, drawY:number, drawW:number, drawH:number):void {
+            this.$drawData.push(sourceX, sourceY, sourceW, sourceH, drawX, drawY, drawW, drawH);
+        }
+        /**
+         * 清空所有缓存的绘制数据
+         */
+        public clean():void{
+            this.$drawData.length = 0;
+        }
+    }
+}
