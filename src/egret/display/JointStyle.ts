aa8049238a3fc878104fecd8e5667ff4c34745ef
dom <dom@domlib.com>
2015-12-11 18:02:47 +0800

    add the JoinStyle class

--- src/egret/display/JointStyle.ts
+++ src/egret/display/JointStyle.ts
@@ -28,61 +28,60 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 module egret {
-
     /**
      * @language en_US
-     * The CapsStyle class is an enumeration of constant values that specify the caps style to use in drawing lines.
-     * The constants are provided for use as values in the caps parameter of the egret.Graphics.lineStyle() method.
+     * The JointStyle class is an enumeration of constant values that specify the joint style to use in drawing lines.
+     * These constants are provided for use as values in the joints parameter of the egret.Graphics.lineStyle() method.
      * @see egret.Graphics#lineStyle()
      * @version Egret 2.5
      * @platform Web,Native
      */
     /**
      * @language zh_CN
-     * CapsStyle 类是可指定在绘制线条中使用的端点样式的常量值枚举。常量可用作 egret.Graphics.lineStyle() 方法的 caps 参数中的值。
+     * JointStyle 类是指定要在绘制线条中使用的联接点样式的常量值枚举。提供的这些常量用作 egret.Graphics.lineStyle() 方法的 joints 参数中的值。
      * @see egret.Graphics#lineStyle()
      * @version Egret 2.5
      * @platform Web,Native
      */
-    export class CapsStyle {
+    export class JointStyle {
         /**
          * @language en_US
-         * Used to specify no caps in the caps parameter of the egret.Graphics.lineStyle() method.
+         * Specifies beveled joints in the joints parameter of the egret.Graphics.lineStyle() method.
          * @version Egret 2.5
          * @platform Web,Native
          */
         /**
          * @language zh_CN
-         * 用于在 egret.Graphics.lineStyle() 方法的 caps 参数中指定没有端点。
+         * 在 egret.Graphics.lineStyle() 方法的 joints 参数中指定斜角连接。
          * @version Egret 2.5
          * @platform Web,Native
          */
-        public static NONE:string = "none";
+        public static BEVEL:string = "bevel";
         /**
          * @language en_US
-         * Used to specify round caps in the caps parameter of the egret.Graphics.lineStyle() method.
+         * Specifies mitered joints in the joints parameter of the egret.Graphics.lineStyle() method.
          * @version Egret 2.5
          * @platform Web,Native
          */
         /**
          * @language zh_CN
-         * 用于在 egret.Graphics.lineStyle() 方法的 caps 参数中指定圆头端点。
+         * 在 egret.Graphics.lineStyle() 方法的 joints 参数中指定尖角连接。
+         * @version Egret 2.5
+         * @platform Web,Native
+         */
+        public static MITER:string = "miter";
+        /**
+         * @language en_US
+         * Specifies round joints in the joints parameter of the egret.Graphics.lineStyle() method.
+         * @version Egret 2.5
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * 在 egret.Graphics.lineStyle() 方法的 joints 参数中指定圆角连接。
          * @version Egret 2.5
          * @platform Web,Native
          */
         public static ROUND:string = "round";
-        /**
-         * @language en_US
-         * Used to specify square caps in the caps parameter of the egret.Graphics.lineStyle() method.
-         * @version Egret 2.5
-         * @platform Web,Native
-         */
-        /**
-         * @language zh_CN
-         * 用于在 egret.Graphics.lineStyle() 方法的 caps 参数中指定方头端点。
-         * @version Egret 2.5
-         * @platform Web,Native
-         */
-        public static SQUARE:string = "square";
     }
 }
