9e48329c8ef30e9115315f95889a4b79aa88262f
himuil <lcj871126@126.com>
2016-03-10 10:21:55 +0800

    fix tsc bug

--- tools/lib/typescript/lib.d.ts
+++ tools/lib/typescript/lib.d.ts
@@ -3841,6 +3841,42 @@ declare var Float64Array: Float64ArrayConstructor;
 /// ECMAScript Internationalization API 
 /////////////////////////////
 
+interface Map<K, V> {
+    clear(): void;
+    delete(key:K): boolean;
+    forEach(callbackfn:(value:V, index:K, map:Map<K, V>) => void, thisArg?:any): void;
+    get(key:K): V;
+    has(key:K): boolean;
+    set(key:K, value:V): Map<K, V>;
+    size: number;
+}
+declare var Map:{
+    new <K, V>(): Map<K, V>;
+}
+
+interface WeakMap<K, V> {
+    clear(): void;
+    delete(key:K): boolean;
+    get(key:K): V;
+    has(key:K): boolean;
+    set(key:K, value:V): WeakMap<K, V>;
+}
+declare var WeakMap:{
+    new <K, V>(): WeakMap<K, V>;
+}
+
+interface Set<T> {
+    add(value:T): Set<T>;
+    clear(): void;
+    delete(value:T): boolean;
+    forEach(callbackfn:(value:T, index:T, set:Set<T>) => void, thisArg?:any): void;
+    has(value:T): boolean;
+    size: number;
+}
+declare var Set:{
+    new <T>(): Set<T>;
+}
+
 declare module Intl {
     interface CollatorOptions {
         usage?: string;
