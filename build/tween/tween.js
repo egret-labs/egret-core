881a951557c6a79410d0e274bd1d5eaf928b855c
f111fei <mail@xzper.com>
2016-08-30 12:13:14 +0800

    Add comment

--- build/tween/tween.js
+++ build/tween/tween.js
@@ -1441,14 +1441,66 @@ var egret;
     egret.Tween = Tween;
     egret.registerClass(Tween,'egret.Tween');
 })(egret || (egret = {}));
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-present, Egret Technology.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
 var egret;
 (function (egret) {
     var tween;
     (function (tween) {
+        /**
+         * @language en_US
+         * Abstract class, Indicate the base action.
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * 抽象类，表示一个基本动作
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
         var BasePath = (function (_super) {
             __extends(BasePath, _super);
             function BasePath() {
                 _super.apply(this, arguments);
+                /**
+                 * @language en_US
+                 * the name of this action.
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 动作的名称
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.name = "";
             }
             var d = __define,c=BasePath,p=c.prototype;
@@ -1456,12 +1508,60 @@ var egret;
         }(egret.EventDispatcher));
         tween.BasePath = BasePath;
         egret.registerClass(BasePath,'egret.tween.BasePath');
+        /**
+         * @language en_US
+         * Indicate the to action. See <code>Tween.to</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * 表示一个to动作，参见<code>Tween.to</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
         var To = (function (_super) {
             __extends(To, _super);
             function To() {
                 _super.apply(this, arguments);
+                /**
+                 * @language en_US
+                 * Property set of an object
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 对象的属性集合
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.props = undefined;
+                /**
+                 * @language en_US
+                 * Duration
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 持续时间
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.duration = 500;
+                /**
+                 * @language en_US
+                 * Easing algorithm
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 缓动算法
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.ease = undefined;
             }
             var d = __define,c=To,p=c.prototype;
@@ -1469,11 +1569,47 @@ var egret;
         }(BasePath));
         tween.To = To;
         egret.registerClass(To,'egret.tween.To');
+        /**
+         * @language en_US
+         * Indicate the wait action. See <code>Tween.wait</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * 表示一个wait动作，参见<code>Tween.wait</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
         var Wait = (function (_super) {
             __extends(Wait, _super);
             function Wait() {
                 _super.apply(this, arguments);
+                /**
+                 * @language en_US
+                 * Duration
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 持续时间
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.duration = 500;
+                /**
+                 * @language en_US
+                 * Whether properties are updated during the waiting time
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 等待期间属性是否会更新
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.passive = undefined;
             }
             var d = __define,c=Wait,p=c.prototype;
@@ -1481,10 +1617,34 @@ var egret;
         }(BasePath));
         tween.Wait = Wait;
         egret.registerClass(Wait,'egret.tween.Wait');
+        /**
+         * @language en_US
+         * Indicate the set action. See <code>Tween.set</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * 表示一个set动作，参见<code>Tween.set</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
         var Set = (function (_super) {
             __extends(Set, _super);
             function Set() {
                 _super.apply(this, arguments);
+                /**
+                 * @language en_US
+                 * Property set of an object
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 对象的属性集合
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 this.props = undefined;
             }
             var d = __define,c=Set,p=c.prototype;
@@ -1492,11 +1652,35 @@ var egret;
         }(BasePath));
         tween.Set = Set;
         egret.registerClass(Set,'egret.tween.Set');
+        /**
+         * @language en_US
+         * Indicate the tick action. See <code>Tween.tick</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * 表示一个tick动作，参见<code>Tween.tick</code>
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
         var Tick = (function (_super) {
             __extends(Tick, _super);
             function Tick() {
                 _super.apply(this, arguments);
-                this.delta = 500;
+                /**
+                 * @language en_US
+                 * Delta time
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * 增加的时间
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                this.delta = 0;
             }
             var d = __define,c=Tick,p=c.prototype;
             return Tick;
@@ -1516,6 +1700,30 @@ var egret;
             return null;
         }
         /**
+         * @language en_US
+         * TweenItem is a wrapper for Tween, which can set the behavior of Tween by setting attributes and adding Path.
+         *
+         * @event pathComplete Dispatched when some Path has complete.
+         * @event complete Dispatched when all Paths has complete.
+         *
+         * @defaultProperty props
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * @language zh_CN
+         * TweenItem是对Tween的包装器，能通过设置属性和添加Path的方式设置Tween的行为。
+         * 通常用于使用在EXML中定义组件的动画。
+         *
+         * @event pathComplete 当某个Path执行完毕时会派发此事件。
+         * @event complete 当所有Path执行完毕时会派发此事件。
+         *
+         * @defaultProperty props
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         */
+        /**
+         * Use in exml:
          * ```
          * 	<tween:TweenItem target="{this.button}">
          * 		<tween:props>
@@ -1546,6 +1754,18 @@ var egret;
             }
             var d = __define,c=TweenItem,p=c.prototype;
             d(p, "props"
+                /**
+                 * @language en_US
+                 * The Tween's props.
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * Tween的props参数。
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 ,function () {
                     return this._props;
                 }
@@ -1554,6 +1774,18 @@ var egret;
                 }
             );
             d(p, "target"
+                /**
+                 * @language en_US
+                 * The Tween's target.
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * Tween的target参数。
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 ,function () {
                     return this._target;
                 }
@@ -1562,6 +1794,18 @@ var egret;
                 }
             );
             d(p, "paths"
+                /**
+                 * @language en_US
+                 * The Actions in Tween.
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * TweenItem中添加的行为。
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 ,function () {
                     return this._paths;
                 }
@@ -1569,6 +1813,18 @@ var egret;
                     this._paths = value || [];
                 }
             );
+            /**
+             * @language en_US
+             * Play the Tween
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
+            /**
+             * @language zh_CN
+             * 播放Tween
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
             p.play = function () {
                 if (!this.tween) {
                     this.createTween();
@@ -1577,6 +1833,18 @@ var egret;
                     this.tween.setPaused(false);
                 }
             };
+            /**
+             * @language en_US
+             * Pause the Tween
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
+            /**
+             * @language zh_CN
+             * 暂停Tween
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
             p.pause = function () {
                 if (this.tween) {
                     this.tween.setPaused(true);
@@ -1615,13 +1883,31 @@ var egret;
                 this.dispatchEventWith('pathComplete', false, path);
                 var index = this._paths.indexOf(path);
                 if (index >= 0 && index === this._paths.length - 1) {
-                    path.dispatchEventWith('complete');
+                    this.dispatchEventWith('complete');
                 }
             };
             return TweenItem;
         }(egret.EventDispatcher));
         tween.TweenItem = TweenItem;
         egret.registerClass(TweenItem,'egret.tween.TweenItem');
+        /**
+         * @language en_US
+         * TweenGroup is a collection of TweenItem that can be played in parallel with each Item
+         *
+         * @event itemComplete Dispatched when some TweenItem has complete.
+         * @event complete Dispatched when all TweenItems has complete.
+         *
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         * @includeExample extension/tween/TweenWrapper.ts
+         */
+        /**
+         * @language zh_CN
+         * TweenGroup是TweenItem的集合，可以并行播放每一个Item
+         * @version Egret 3.1.8
+         * @platform Web,Native
+         * @includeExample extension/tween/TweenWrapper.ts
+         */
         var TweenGroup = (function (_super) {
             __extends(TweenGroup, _super);
             function TweenGroup() {
@@ -1630,6 +1916,18 @@ var egret;
             }
             var d = __define,c=TweenGroup,p=c.prototype;
             d(p, "items"
+                /**
+                 * @language en_US
+                 * The Array that TweenItems in TweenGroup.
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
+                /**
+                 * @language zh_CN
+                 * TweenGroup要控制的TweenItem集合。
+                 * @version Egret 3.1.8
+                 * @platform Web,Native
+                 */
                 ,function () {
                     return this._items;
                 }
@@ -1651,6 +1949,18 @@ var egret;
                     }
                 });
             };
+            /**
+             * @language en_US
+             * Play the all TweenItems
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
+            /**
+             * @language zh_CN
+             * 播放所有的TweenItem
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
             p.play = function () {
                 if (this._items) {
                     for (var i = 0; i < this._items.length; i++) {
@@ -1659,6 +1969,18 @@ var egret;
                     }
                 }
             };
+            /**
+             * @language en_US
+             * Pause the all TweenItems
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
+            /**
+             * @language zh_CN
+             * 暂停播放所有的TweenItem
+             * @version Egret 3.1.8
+             * @platform Web,Native
+             */
             p.pause = function () {
                 if (this._items) {
                     for (var i = 0; i < this._items.length; i++) {
@@ -1668,7 +1990,7 @@ var egret;
                 }
             };
             p.itemComplete = function (e) {
-                var item = e.currentTarget.data;
+                var item = e.currentTarget;
                 this.completeCount++;
                 this.dispatchEventWith('itemComplete', false, item);
                 if (this.completeCount === this.items.length) {
