6f83dfd9b2f7f63a0b01fca00c7e1f374d75ebeb
WanderWang <wangze@egret.com>
2017-01-16 17:26:22 +0800

    fix upgrade error

--- tools/commands/upgrade.js
+++ tools/commands/upgrade.js
@@ -5,43 +5,9 @@
 /// <reference path="../lib/async/async.d.ts" />
 var async = require("../lib/async/async");
 var service = require("../service/index");
-//import config = require('../lib/ProjectConfig');
-//import globals = require("../Globals");
 var UpgradeCommand = (function () {
     function UpgradeCommand() {
-        this.upgradeConfigArr = [
-            //todo
-            { "v": "1.0.3", "command": require("./upgrade/UpgradeCommand_1_0_3") },
-            { "v": "1.0.4", "command": require("./upgrade/UpgradeCommand_1_0_4") },
-            { "v": "1.0.5", "command": require("./upgrade/UpgradeCommand_1_0_5") },
-            //{"v" : "1.0.6"},
-            { "v": "1.1.0", "command": require("./upgrade/UpgradeCommand_1_1_0") },
-            //{"v" : "1.1.1"},
-            //{"v" : "1.1.2"},
-            //{"v" : "1.1.3"},
-            //{"v" : "1.1.4"},
-            { "v": "1.5.0", "command": require("./upgrade/UpgradeCommand_1_5_0") },
-            { "v": "1.5.1", "command": require("./upgrade/UpgradeCommand_1_5_1") },
-            { "v": "1.5.2", "command": require("./upgrade/UpgradeCommand_1_5_2") },
-            //{"v" : "1.5.3"},
-            //{"v" : "1.5.4"},
-            { "v": "1.5.5", "command": require("./upgrade/UpgradeCommand_1_5_5") },
-            //{"v" : "1.6.0"},
-            //{"v" : "1.6.1"},
-            //{"v" : "1.6.2"},
-            //{"v" : "1.7.0"},
-            //{"v" : "1.7.1"},
-            //{"v": "1.7.2"},
-            { "v": "1.7.3", "command": require("./upgrade/UpgradeCommand_1_7_3") },
-            //{"v": "2.0.0"},
-            //{"v": "2.0.1"},
-            //{"v": "2.0.2"},
-            //{"v": "2.4.0"},
-            //{"v": "2.4.1"},
-            { "v": "2.5.0", "command": require("./upgrade/UpgradeCommand_2_4_3") },
-            { "v": "2.5.1", "command": require("./upgrade/UpgradeCommand_2_5_1") },
-            { "v": "4.0.0" }
-        ];
+        this.upgradeConfigArr = [];
     }
     UpgradeCommand.prototype.execute = function () {
         this.run();
@@ -66,10 +32,10 @@ var UpgradeCommand = (function () {
                     callback({ name: "", message: err });
                 }
             }
-            var v = info["v"];
+            var v = info.v;
             var command;
-            if (info["command"]) {
-                command = new info["command"]();
+            if (info.command) {
+                command = new info.command();
             }
             var result = globals.compressVersion(version, v);
             if (result < 0) {
--- tools/commands/upgrade.ts
+++ tools/commands/upgrade.ts
@@ -12,12 +12,19 @@ import service = require('../service/index');
 //import config = require('../lib/ProjectConfig');
 //import globals = require("../Globals");
 
+
+type VersionInfo = {
+
+    v: string,
+    command: { new (): egret.Command }
+}
+
 class UpgradeCommand implements egret.Command {
 
-    constructor(){
+    constructor() {
     }
 
-    execute():number {
+    execute(): number {
         this.run();
         return DontExitCode
     }
@@ -32,47 +39,47 @@ class UpgradeCommand implements egret.Command {
             version = "1.0.0";
         }
         var modify = require("./upgrade/ModifyProperties");
-        async.eachSeries(this.upgradeConfigArr,function(info,callback){
+        async.eachSeries(this.upgradeConfigArr, function (info, callback) {
 
-            function handleCallBack(err?:string){
-                if(!err){
+            function handleCallBack(err?: string) {
+                if (!err) {
                     modify.save(v);
                     callback();
-                }else{
-                    callback({name:"",message:err});
+                } else {
+                    callback({ name: "", message: err });
                 }
             }
 
-            var v = info["v"];
+            var v = info.v;
             var command;
-            if(info["command"]) {
-                command = new info["command"]();
+            if (info.command) {
+                command = new info.command();
             }
 
             var result = globals.compressVersion(version, v);
 
             if (result < 0) {
                 globals.log(1704, v);
-                if(!command){
+                if (!command) {
                     handleCallBack();
-                }else
-                if(command.isAsync){
-                    command.execute(handleCallBack);
-                }else{
-                    var upgradeCommandError = command.execute();
-                    if(upgradeCommandError != 0){
-                        handleCallBack('升级中断');
-                    }else{
-                        handleCallBack();
+                } else
+                    if (command.isAsync) {
+                        command.execute(handleCallBack);
+                    } else {
+                        var upgradeCommandError = command.execute();
+                        if (upgradeCommandError != 0) {
+                            handleCallBack('升级中断');
+                        } else {
+                            handleCallBack();
+                        }
                     }
-                }
-            }else{
+            } else {
                 callback();
             }
-        },function(error){
-            if(error){
+        }, function (error) {
+            if (error) {
                 globals.exit(1705);
-            }else{
+            } else {
                 service.execCommand({
                     path: egret.args.projectDir,
                     command: "shutdown",
@@ -105,38 +112,8 @@ class UpgradeCommand implements egret.Command {
         //globals.exit(1702);
     }
 
-    private upgradeConfigArr = [
-        //todo
-        {"v": "1.0.3", "command": require("./upgrade/UpgradeCommand_1_0_3")},
-        {"v": "1.0.4", "command": require("./upgrade/UpgradeCommand_1_0_4")},
-        {"v": "1.0.5", "command": require("./upgrade/UpgradeCommand_1_0_5")},
-        //{"v" : "1.0.6"},
-        {"v": "1.1.0", "command": require("./upgrade/UpgradeCommand_1_1_0")},
-        //{"v" : "1.1.1"},
-        //{"v" : "1.1.2"},
-        //{"v" : "1.1.3"},
-        //{"v" : "1.1.4"},
-        {"v": "1.5.0", "command": require("./upgrade/UpgradeCommand_1_5_0")},
-        {"v": "1.5.1", "command": require("./upgrade/UpgradeCommand_1_5_1")},
-        {"v": "1.5.2", "command": require("./upgrade/UpgradeCommand_1_5_2")},
-        //{"v" : "1.5.3"},
-        //{"v" : "1.5.4"},
-        {"v": "1.5.5", "command": require("./upgrade/UpgradeCommand_1_5_5")},
-        //{"v" : "1.6.0"},
-        //{"v" : "1.6.1"},
-        //{"v" : "1.6.2"},
-        //{"v" : "1.7.0"},
-        //{"v" : "1.7.1"},
-        //{"v": "1.7.2"},
-        {"v": "1.7.3", "command": require("./upgrade/UpgradeCommand_1_7_3")},
-        //{"v": "2.0.0"},
-        //{"v": "2.0.1"},
-        //{"v": "2.0.2"},
-        //{"v": "2.4.0"},
-        //{"v": "2.4.1"},
-        {"v": "2.5.0", "command": require("./upgrade/UpgradeCommand_2_4_3")},
-        {"v": "2.5.1", "command": require("./upgrade/UpgradeCommand_2_5_1")},
-        {"v": "4.0.0"}
+    private upgradeConfigArr: VersionInfo[] = [
+        // {"v": "1.0.5", "command": require("./upgrade/UpgradeCommand_1_0_5")},
     ];
 }
 
