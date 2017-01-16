48013520f06dab31b2b72571c1b5102537b5c98f
WanderWang <wangze@egret.com>
2017-01-16 18:28:16 +0800

    refactor egret upgrade

--- tools/commands/upgrade.js
+++ tools/commands/upgrade.js
@@ -2,120 +2,154 @@
  * Created by yanjiaqi on 15/9/7.
  */
 /// <reference path="../lib/types.d.ts" />
-/// <reference path="../lib/async/async.d.ts" />
-var async = require("../lib/async/async");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var service = require("../service/index");
 var UpgradeCommand = (function () {
     function UpgradeCommand() {
-        this.upgradeConfigArr = [
-            { "v": "4.0.0" }
-        ];
     }
     UpgradeCommand.prototype.execute = function () {
         this.run();
         return DontExitCode;
     };
     UpgradeCommand.prototype.run = function () {
-        //var currDir = params.getProjectRoot();
-        //var config = require("../core/projectConfig.js");
-        //config.init(currDir);
-        var version = egret.args.properties.getVersion();
-        if (!version) {
-            version = "1.0.0";
-        }
-        var modify = require("./upgrade/ModifyProperties");
-        async.eachSeries(this.upgradeConfigArr, function (info, callback) {
-            function handleCallBack(err) {
-                if (!err) {
-                    modify.save(v);
-                    callback();
+        return __awaiter(this, void 0, void 0, function () {
+            var version, modify, upgradeConfigArr, e_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        version = egret.args.properties.getVersion();
+                        if (!version) {
+                            version = "1.0.0";
+                        }
+                        modify = require("./upgrade/ModifyProperties");
+                        upgradeConfigArr = [
+                            { "v": "4.0.1", command: Upgrade_4_0_1 }
+                        ];
+                        _a.label = 1;
+                    case 1:
+                        _a.trys.push([1, 3, , 4]);
+                        return [4 /*yield*/, series(upgrade, upgradeConfigArr.concat())];
+                    case 2:
+                        _a.sent();
+                        modify.save(upgradeConfigArr.pop().v);
+                        service.execCommand({
+                            path: egret.args.projectDir,
+                            command: "shutdown",
+                            option: egret.args
+                        }, function (e) {
+                            globals.log(1702);
+                            return globals.exit(0);
+                        }, true);
+                        return [3 /*break*/, 4];
+                    case 3:
+                        e_1 = _a.sent();
+                        console.log(e_1);
+                        globals.exit(1705);
+                        return [3 /*break*/, 4];
+                    case 4: return [2 /*return*/];
                 }
-                else {
-                    callback({ name: "", message: err });
-                }
-            }
-            var v = info.v;
-            var command;
-            if (info.command) {
-                command = new info.command();
-            }
-            var result = globals.compressVersion(version, v);
-            if (result < 0) {
-                globals.log(1704, v);
-                if (!command) {
-                    handleCallBack();
-                }
-                else if (command.isAsync) {
-                    command.execute(handleCallBack);
-                }
-                else {
-                    var upgradeCommandError = command.execute();
-                    if (upgradeCommandError != 0) {
-                        handleCallBack('升级中断');
-                    }
-                    else {
-                        handleCallBack();
-                    }
-                }
-            }
-            else {
-                callback();
-            }
-        }, function (error) {
-            if (error) {
-                globals.exit(1705);
-            }
-            else {
-                service.execCommand({
-                    path: egret.args.projectDir,
-                    command: "shutdown",
-                    option: egret.args
-                }, function () {
-                    globals.log(1702);
-                    return globals.exit(0);
-                }, true);
-            }
+            });
         });
-        //for (var i = 0; i < this.upgradeConfigArr.length; i++) {
-        //    var info = this.upgradeConfigArr[i];
-        //    var v = info["v"];
-        //    var command = new info["command"]();
-        //
-        //    var result = globals.compressVersion(version, v);
-        //    if (result < 0) {
-        //        globals.log(1704, v);
-        //        var upgradeCommandError = 0;
-        //        if (command) {
-        //            upgradeCommandError = command.execute();
-        //        }
-        //        if (!upgradeCommandError){
-        //            modify.save(v);
-        //        }
-        //    }
-        //}
-        //
-        //globals.exit(1702);
     };
     return UpgradeCommand;
 }());
-/**
- * todo
- */
-function executeCommands(commands, callback) {
-    if (commands.length == 0) {
-        callback();
+var series = function (cb, arr) {
+    var parallel = 1;
+    // allow default parallel count of 1 if array
+    // passed as second param (this a good idea?)
+    if (Array.isArray(parallel)) {
+        arr = parallel;
+        parallel = 1;
     }
-    else {
-        var command = commands.shift();
-        var result = command.execute();
-        if (typeof (result) == 'number') {
-            executeCommands(commands, callback);
+    var results = [];
+    var promises = [];
+    for (var i = 0; i < parallel; i++)
+        promises.push(Promise.resolve());
+    arr.forEach(function (item, ix) {
+        var position = ix % parallel;
+        var promise = promises[position];
+        promises[position] = promise.then(function () {
+            return Promise.resolve(cb(item, ix, results))
+                .then(function (res) { return results.push(res); });
+        });
+    });
+    return Promise.all(promises)
+        .then(function () { return results; });
+};
+function upgrade(info) {
+    var version = egret.args.properties.getVersion();
+    var v = info.v;
+    var command;
+    if (info.command) {
+        command = new info.command();
+    }
+    var result = globals.compressVersion(version, v);
+    if (result < 0) {
+        globals.log(1704, v);
+        if (!command) {
+            return Promise.resolve(0);
         }
         else {
-            result.then(function () {
-                executeCommands(commands, callback);
-            });
+            var commandPromise = command.execute();
+            if (typeof commandPromise == 'number') {
+                console.error('internal error !!!');
+            }
+            else {
+                return new Promise(function (reslove, reject) {
+                    var p = commandPromise;
+                    p.then(function () {
+                        reslove(0);
+                    }).catch(function () {
+                        reject('升级中断');
+                    });
+                });
+            }
         }
     }
+    else {
+        return Promise.resolve(0);
+    }
 }
+var Upgrade_4_0_1 = (function () {
+    function Upgrade_4_0_1() {
+    }
+    Upgrade_4_0_1.prototype.execute = function () {
+        return Promise.resolve(0);
+    };
+    return Upgrade_4_0_1;
+}());
 module.exports = UpgradeCommand;
--- tools/commands/upgrade.ts
+++ tools/commands/upgrade.ts
@@ -2,12 +2,10 @@
  * Created by yanjiaqi on 15/9/7.
  */
 /// <reference path="../lib/types.d.ts" />
-/// <reference path="../lib/async/async.d.ts" />
 
 
 //import params = require("../ParamsParser");
 import file = require('../lib/FileUtil');
-import async = require('../lib/async/async');
 import service = require('../service/index');
 //import config = require('../lib/ProjectConfig');
 //import globals = require("../Globals");
@@ -29,7 +27,7 @@ class UpgradeCommand implements egret.Command {
         return DontExitCode
     }
 
-    private run() {
+    private async run() {
         //var currDir = params.getProjectRoot();
 
         //var config = require("../core/projectConfig.js");
@@ -39,105 +37,105 @@ class UpgradeCommand implements egret.Command {
             version = "1.0.0";
         }
         var modify = require("./upgrade/ModifyProperties");
-        async.eachSeries(this.upgradeConfigArr, function (info, callback) {
 
-            function handleCallBack(err?: string) {
-                if (!err) {
-                    modify.save(v);
-                    callback();
-                } else {
-                    callback({ name: "", message: err });
-                }
-            }
+        let upgradeConfigArr: VersionInfo[] = [
+            { "v": "4.0.1", command: Upgrade_4_0_1 }
+        ];
 
-            var v = info.v;
-            var command;
-            if (info.command) {
-                command = new info.command();
-            }
+        try {
+            await series(upgrade, upgradeConfigArr.concat())
+            modify.save(upgradeConfigArr.pop().v);
+            service.execCommand({
+                path: egret.args.projectDir,
+                command: "shutdown",
+                option: egret.args
+            }, (e) => {
+                globals.log(1702);
+                return globals.exit(0);
+            }, true);
+        }
+        catch (e) {
+            console.log(e)
+            globals.exit(1705);
+        }
+    }
+}
 
-            var result = globals.compressVersion(version, v);
 
-            if (result < 0) {
-                globals.log(1704, v);
-                if (!command) {
-                    handleCallBack();
-                } else
-                    if (command.isAsync) {
-                        command.execute(handleCallBack);
-                    } else {
-                        var upgradeCommandError = command.execute();
-                        if (upgradeCommandError != 0) {
-                            handleCallBack('升级中断');
-                        } else {
-                            handleCallBack();
-                        }
-                    }
-            } else {
-                callback();
-            }
-        }, function (error) {
-            if (error) {
-                globals.exit(1705);
-            } else {
-                service.execCommand({
-                    path: egret.args.projectDir,
-                    command: "shutdown",
-                    option: egret.args
-                }, () => {
-                    globals.log(1702);
-                    return globals.exit(0);
-                }, true);
-            }
+let series = <T>(cb: (data: T, index?: number, result?: any) => Promise<number>, arr: T[]) => {
+
+    let parallel = 1;
+
+    // allow default parallel count of 1 if array
+    // passed as second param (this a good idea?)
+    if (Array.isArray(parallel)) {
+        arr = parallel;
+        parallel = 1;
+    }
+
+    const results = [];
+    const promises = [];
+
+    for (var i = 0; i < parallel; i++)
+        promises.push(Promise.resolve());
+
+    arr.forEach((item, ix) => {
+
+        const position = ix % parallel;
+        const promise = promises[position];
+
+        promises[position] = promise.then(() => {
+            return Promise.resolve(cb(item, ix, results))
+                .then(res => results.push(res));
         });
+    });
 
-        //for (var i = 0; i < this.upgradeConfigArr.length; i++) {
-        //    var info = this.upgradeConfigArr[i];
-        //    var v = info["v"];
-        //    var command = new info["command"]();
-        //
-        //    var result = globals.compressVersion(version, v);
-        //    if (result < 0) {
-        //        globals.log(1704, v);
-        //        var upgradeCommandError = 0;
-        //        if (command) {
-        //            upgradeCommandError = command.execute();
-        //        }
-        //        if (!upgradeCommandError){
-        //            modify.save(v);
-        //        }
-        //    }
-        //}
-        //
-        //globals.exit(1702);
-    }
-
-    private upgradeConfigArr: VersionInfo[] = [
-        { "v": "4.0.0" }
-    ];
+    return Promise.all(promises)
+        .then(() => results);
 }
 
-/**
- * todo
- */
-function executeCommands(commands: egret.Command[], callback) {
-    if (commands.length == 0) {
-        callback();
+function upgrade(info: VersionInfo) {
+    var version = egret.args.properties.getVersion();
+    var v = info.v;
+    var command: egret.Command;
+    if (info.command) {
+        command = new info.command();
     }
-    else {
-        let command = commands.shift();
-        let result = command.execute();
-        if (typeof (result) == 'number') {
-            executeCommands(commands, callback);
-        }
-        else {
-            result.then(() => {
-                executeCommands(commands, callback)
-            })
-        }
-    }
+    var result = globals.compressVersion(version, v);
+    if (result < 0) {
+        globals.log(1704, v);
+        if (!command) {
+            return Promise.resolve(0);
+        } else {
+            var commandPromise = command.execute();
+            if (typeof commandPromise == 'number') {
+                console.error('internal error !!!')
+            }
+            else {
+                return new Promise((reslove, reject) => {
+                    let p = commandPromise as Promise<number>
+                    p.then(() => {
+                        reslove(0)
+                    }).catch(() => {
+                        reject('升级中断')
+                    })
+                })
+            }
 
+        }
+
+    } else {
+        return Promise.resolve(0)
+    }
 }
 
+
+class Upgrade_4_0_1 {
+
+
+    execute() {
+        return Promise.resolve(0)
+    }
+}
 
 export = UpgradeCommand;
