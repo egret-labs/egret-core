4ed56df71d1a464fd5f5ffc48a9bfbc788fd0796
WanderWang <wangze@egret.com>
2017-01-17 15:31:50 +0800

    refactor copyfiles

--- tools/actions/CopyFiles.js
+++ tools/actions/CopyFiles.js
@@ -1,44 +1,28 @@
 /// <reference path="../lib/types.d.ts" />
 var FileUtil = require("../lib/FileUtil");
+var EgretProject = require("../parser/EgretProject");
 var fileExtensionToIgnore = {
     "ts": true
 };
 function copyToLibs() {
     var options = egret.args;
-    var exitsModules = [];
     var moduleDir = FileUtil.joinPath(options.libsDir, 'modules');
-    var list = FileUtil.getDirectoryListing(moduleDir);
-    for (var i = 0; i < list.length; i++) {
-        if (FileUtil.isDirectory(list[i])) {
-            exitsModules.push(FileUtil.getRelativePath(moduleDir, list[i]));
-        }
-    }
-    var properties = egret.args.properties;
-    var modules = properties.getAllModuleNames();
-    for (var tempK in modules) {
-        var moduleName = modules[tempK];
+    FileUtil.remove(moduleDir);
+    var properties = EgretProject.utils;
+    properties.getAllModuleNames().map(function (moduleName) {
         var modulePath = properties.getModulePath(moduleName);
+        var moduleBin;
         if (modulePath == null) {
-            var moduleBin = FileUtil.joinPath(egret.root, "build", moduleName);
+            moduleBin = FileUtil.joinPath(egret.root, "build", moduleName);
         }
         else {
             var tempModulePath = FileUtil.getAbsolutePath(modulePath);
             moduleBin = FileUtil.joinPath(tempModulePath, "bin", moduleName);
         }
-        var targetFile = FileUtil.joinPath(options.libsDir, 'modules', moduleName);
+        var targetFile = FileUtil.joinPath(moduleDir, moduleName);
         if (options.projectDir.toLowerCase() != egret.root.toLowerCase()) {
             FileUtil.copy(moduleBin, targetFile);
         }
-        var index = exitsModules.indexOf(moduleName);
-        if (index != -1) {
-            exitsModules.splice(index, 1);
-        }
-    }
-    var length = exitsModules.length;
-    if (length > 0) {
-        for (var i = 0; i < exitsModules.length; i++) {
-            FileUtil.remove(FileUtil.joinPath(moduleDir, exitsModules[i]));
-        }
-    }
+    });
 }
 exports.copyToLibs = copyToLibs;
--- tools/actions/CopyFiles.ts
+++ tools/actions/CopyFiles.ts
@@ -5,6 +5,7 @@ import utils = require('../lib/utils');
 import Compiler = require('./Compiler');
 import FileUtil = require('../lib/FileUtil');
 import CompileTemplate = require('./CompileTemplate');
+import * as EgretProject from '../parser/EgretProject';
 
 var fileExtensionToIgnore = {
     "ts": true
@@ -12,48 +13,22 @@ var fileExtensionToIgnore = {
 
 export function copyToLibs() {
     var options = egret.args;
-    var exitsModules = [];
-    var moduleDir = FileUtil.joinPath(options.libsDir, 'modules');
-    var list = FileUtil.getDirectoryListing(moduleDir);
-    for (var i = 0; i < list.length; i++) {
-        if (FileUtil.isDirectory(list[i])) {
-            exitsModules.push(FileUtil.getRelativePath(moduleDir, list[i]));
-        }
-    }
-
-    var properties = egret.args.properties;
-    var modules = properties.getAllModuleNames();
-
-    for (var tempK in modules) {
-        var moduleName = modules[tempK];
-        var modulePath = properties.getModulePath(moduleName);
+    let moduleDir = FileUtil.joinPath(options.libsDir, 'modules');
+    FileUtil.remove(moduleDir);
+    let properties = EgretProject.utils;
+    properties.getAllModuleNames().map(moduleName => {
+        let modulePath = properties.getModulePath(moduleName);
+        let moduleBin;
         if (modulePath == null) {
-            var moduleBin = FileUtil.joinPath(egret.root, "build", moduleName);
+            moduleBin = FileUtil.joinPath(egret.root, "build", moduleName);
         }
         else {
-            var tempModulePath = FileUtil.getAbsolutePath(modulePath);
+            let tempModulePath = FileUtil.getAbsolutePath(modulePath);
             moduleBin = FileUtil.joinPath(tempModulePath, "bin", moduleName);
-
-            //if (FileUtil.exists(FileUtil.joinPath(modulePath))) {
-            //    moduleBin = FileUtil.joinPath(modulePath, "bin", moduleName);
-            //}
-            //else {
-            //    moduleBin = FileUtil.joinPath(options.projectDir, modulePath, "bin", moduleName);
-            //}
         }
-        var targetFile = FileUtil.joinPath(options.libsDir, 'modules', moduleName);
+        let targetFile = FileUtil.joinPath(moduleDir, moduleName);
         if (options.projectDir.toLowerCase() != egret.root.toLowerCase()) {
             FileUtil.copy(moduleBin, targetFile);
         }
-        var index = exitsModules.indexOf(moduleName);
-        if (index != -1) {
-            exitsModules.splice(index, 1);
-        }
-    }
-    var length = exitsModules.length;
-    if (length > 0) {
-        for (var i = 0; i < exitsModules.length; i++) {
-            FileUtil.remove(FileUtil.joinPath(moduleDir, exitsModules[i]));
-        }
-    }
+    })
 }
