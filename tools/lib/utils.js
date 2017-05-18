ee5d75c734af0be8df3da4a26b99a688107aa58f
王泽 <wangze@egret.com>
2017-05-18 22:54:05 +0800

    add resourcemanager publish

--- tools/lib/utils.js
+++ tools/lib/utils.js
@@ -31,6 +31,41 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var cp = require("child_process");
 var path = require("path");
 var file = require("./FileUtil");
@@ -157,44 +192,52 @@ function getEgretRoot() {
     return file.escapePath(file.joinPath(egretRoot, '/'));
 }
 exports.getEgretRoot = getEgretRoot;
-function open(target, appName, callback, options) {
-    var opener;
-    if (typeof (appName) === 'function') {
-        callback = appName;
-        appName = null;
-    }
+function open(target, appName) {
+    var command;
     switch (process.platform) {
         case 'darwin':
             if (appName) {
-                opener = 'open -a "' + escape(appName) + '"';
+                command = 'open -a "' + escape(appName) + '"';
             }
             else {
-                opener = 'open';
+                command = 'open';
             }
             break;
         case 'win32':
             // if the first parameter to start is quoted, it uses that as the title
             // so we pass a blank title so we can quote the file we are opening
             if (appName) {
-                opener = 'start "" "' + escape(appName) + '"';
+                command = 'start "" "' + escape(appName) + '"';
             }
             else {
-                opener = 'start ""';
+                command = 'start ""';
             }
             break;
         default:
             if (appName) {
-                opener = escape(appName);
+                command = escape(appName);
             }
             else {
                 // use Portlands xdg-open everywhere else
-                opener = path.join(__dirname, '../vendor/xdg-open');
+                command = path.join(__dirname, '../vendor/xdg-open');
             }
             break;
     }
-    return cp.exec(opener + ' "' + escape(target) + '"', options, callback);
+    executeCommand(command + ' "' + escape(target) + '"');
 }
 exports.open = open;
+function executeCommand(command) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/, new Promise(function (reslove, reject) {
+                    cp.exec(command, {}, function (error, stdout, stderr) {
+                        reslove({ error: error, stdout: stdout, stderr: stderr });
+                    });
+                })];
+        });
+    });
+}
+exports.executeCommand = executeCommand;
 function endWith(text, match) {
     return text.lastIndexOf(match) == (text.length - match.length);
 }
