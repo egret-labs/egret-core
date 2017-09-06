604a99f9b9606ee213673851284064aeadee22b9
himuil <jerry.lin@egret.com>
2017-09-06 13:46:16 +0800

    Fix eui.TileLayout bug

--- build/eui/eui.js
+++ build/eui/eui.js
@@ -23894,9 +23894,6 @@ var eui;
                     case egret.HorizontalAlign.RIGHT:
                         x = width - (columnIndex + 1) * (columnWidth + horizontalGap) + horizontalGap - paddingR;
                         break;
-                    case egret.HorizontalAlign.CENTER:
-                        x = width / 2 - (columnCount * columnWidth + (columnCount - 1) * horizontalGap) / 2 + columnIndex * (columnWidth + horizontalGap);
-                        break;
                     case egret.HorizontalAlign.LEFT:
                         x = columnIndex * (columnWidth + horizontalGap) + paddingL;
                         break;
@@ -23909,9 +23906,6 @@ var eui;
                         break;
                     case egret.VerticalAlign.BOTTOM:
                         y = height - (rowIndex + 1) * (rowHeight + verticalGap) + verticalGap - paddingB;
-                        break;
-                    case egret.VerticalAlign.MIDDLE:
-                        y = height / 2 - (rowCount * rowHeight + (rowCount - 1) * verticalGap) / 2 + rowIndex * (rowHeight + verticalGap);
                         break;
                     default:
                         y = rowIndex * (rowHeight + verticalGap) + paddingT;
--- src/extension/eui/layouts/TileLayout.ts
+++ src/extension/eui/layouts/TileLayout.ts
@@ -1157,9 +1157,6 @@ namespace eui {
                     case egret.HorizontalAlign.RIGHT:
                         x = width - (columnIndex + 1) * (columnWidth + horizontalGap) + horizontalGap - paddingR;
                         break;
-                    case egret.HorizontalAlign.CENTER:
-                        x = width / 2 -  (columnCount * columnWidth + (columnCount - 1) * horizontalGap) / 2 + columnIndex * (columnWidth + horizontalGap);
-                        break;
                     case egret.HorizontalAlign.LEFT:
                         x = columnIndex * (columnWidth + horizontalGap) + paddingL;
                         break;
@@ -1173,9 +1170,6 @@ namespace eui {
                         break;
                     case egret.VerticalAlign.BOTTOM:
                         y = height - (rowIndex + 1) * (rowHeight + verticalGap) + verticalGap - paddingB;
-                        break;
-                    case egret.VerticalAlign.MIDDLE:
-                        y = height / 2 -  (rowCount * rowHeight + (rowCount - 1) * verticalGap) / 2 + rowIndex * (rowHeight + verticalGap);
                         break;
                     default:
                         y = rowIndex * (rowHeight + verticalGap) + paddingT;
