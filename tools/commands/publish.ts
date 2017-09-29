be44a20f494b8351d3707a1a395b4db6d534fea7
WanderWang <185713114@qq.com>
2017-09-29 14:28:00 +0800

    refactor prepare

--- tools/commands/publish.ts
+++ tools/commands/publish.ts
@@ -8,56 +8,45 @@ import FileUtil = require('../lib/FileUtil');
 import CompileProject = require('../actions/CompileProject');
 import { publishResource, legacyPublishHTML5, legacyPublishNative } from '../actions/PublishResourceAction';
 import ZipCommand = require("../actions/ZipCommand");
-import ChangeEntranceCMD = require("../actions/ChangeEntranceCommand");
 
 import project = require("../actions/Project");
 import EgretProject = require("../project/EgretProject");
 
+import * as path from 'path';
 
 
-import Clean = require("../commands/clean");
-
-import path = require('path');
 
 class Publish implements egret.Command {
-    private getVersionInfo(): string {
-        if (egret.args.version) {
-            return egret.args.version;
-        }
-
-        var date = new Date();
-        var year = date.getFullYear() % 100;
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var hour = date.getHours();
-        var min = date.getMinutes();
-        var second = date.getSeconds();
-        var timeStr = year * 10000000000 + month * 100000000 + day * 1000000 + hour * 10000 + min * 100 + second;
-        return timeStr.toString();
-
-        //return (Math.round(Date.now() / 1000)).toString();
-    }
+    private
 
     async execute(): Promise<number> {
         utils.checkEgret();
 
-        var options = egret.args;
-        var config = EgretProject.data;
-        //重新设置 releaseDir
-        var versionFile = this.getVersionInfo();
-
+        const options = egret.args;
+        const config = EgretProject.data;
+        const versionFile = getVersionInfo();
         const runtime = egret.args.runtime == 'native' ? 'native' : "web";
         options.releaseDir = FileUtil.joinPath(config.getReleaseRoot(), runtime, versionFile);
         globals.log(1402, runtime, versionFile);
-
         await publishResource(versionFile, runtime);
-
-
         return DontExitCode;
     }
-
 }
 
+function getVersionInfo() {
+    if (egret.args.version) {
+        return egret.args.version;
+    }
 
+    var date = new Date();
+    var year = date.getFullYear() % 100;
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var second = date.getSeconds();
+    var timeStr = year * 10000000000 + month * 100000000 + day * 1000000 + hour * 10000 + min * 100 + second;
+    return timeStr.toString();
+}
 
 export = Publish;
