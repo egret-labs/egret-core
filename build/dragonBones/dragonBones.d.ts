5f3d2e32e5634b6a131fb6e633b42cf685afecae
akdcl <akdcl@sina.com>
2017-10-09 10:14:26 +0800

    update dragonbones js

--- build/dragonBones/dragonBones.d.ts
+++ build/dragonBones/dragonBones.d.ts
@@ -622,6 +622,10 @@ declare namespace dragonBones {
         /**
          * @private
          */
+        stage: ArmatureData | null;
+        /**
+         * @private
+         */
         readonly frameIndices: Array<number>;
         /**
          * @private
@@ -3844,6 +3848,7 @@ declare namespace dragonBones {
         protected static readonly NAME: string;
         protected static readonly PARENT: string;
         protected static readonly TARGET: string;
+        protected static readonly STAGE: string;
         protected static readonly SHARE: string;
         protected static readonly PATH: string;
         protected static readonly LENGTH: string;
@@ -3950,7 +3955,7 @@ declare namespace dragonBones {
         protected _animation: AnimationData;
         protected _timeline: TimelineData;
         protected _rawTextureAtlases: Array<any> | null;
-        private _defalultColorOffset;
+        private _defaultColorOffset;
         private _prevClockwise;
         private _prevRotation;
         private readonly _helpMatrixA;
@@ -3966,11 +3971,11 @@ declare namespace dragonBones {
         private readonly _frameArray;
         private readonly _timelineArray;
         private readonly _actionFrames;
+        private readonly _meshs;
         private readonly _weightSlotPose;
         private readonly _weightBonePoses;
         private readonly _cacheBones;
-        private readonly _meshs;
-        private readonly _shareMeshs;
+        private readonly _cacheMeshs;
         private readonly _slotChildActions;
         /**
          * @private
@@ -3983,10 +3988,6 @@ declare namespace dragonBones {
         private _sortActionFrame(a, b);
         private _parseActionDataInFrame(rawData, frameStart, bone, slot);
         private _mergeActionFrame(rawData, frameStart, type, bone, slot);
-        private _parseCacheActionFrame(frame);
-        /**
-         * @private
-         */
         protected _parseArmature(rawData: any, scale: number): ArmatureData;
         /**
          * @private
@@ -4048,9 +4049,7 @@ declare namespace dragonBones {
          * @private
          */
         protected _parseTweenFrame(rawData: any, frameStart: number, frameCount: number): number;
-        /**
-         * @private
-         */
+        private _parseActionFrame(frame, frameStart, frameCount);
         protected _parseZOrderFrame(rawData: any, frameStart: number, frameCount: number): number;
         /**
          * @private
@@ -4465,6 +4464,7 @@ declare namespace dragonBones {
      */
     class EgretTextureAtlasData extends TextureAtlasData {
         static toString(): string;
+        disposeEnabled: boolean;
         private _renderTexture;
         /**
          * @private
@@ -4716,18 +4716,6 @@ declare namespace dragonBones {
         /**
          * @inheritDoc
          */
-        $getWidth(): number;
-        /**
-         * @inheritDoc
-         */
-        $getHeight(): number;
-        /**
-         * @inheritDoc
-         */
-        $hitTest(stageX: number, stageY: number): egret.DisplayObject;
-        /**
-         * @inheritDoc
-         */
         $measureContentBounds(bounds: egret.Rectangle): void;
         /**
          * @deprecated
@@ -4957,7 +4945,7 @@ declare namespace dragonBones {
         /**
          * @private
          */
-        protected _buildTextureAtlasData(textureAtlasData: EgretTextureAtlasData | null, textureAtlas: egret.Texture | null): EgretTextureAtlasData;
+        protected _buildTextureAtlasData(textureAtlasData: EgretTextureAtlasData | null, textureAtlas: egret.Texture | HTMLImageElement | null): EgretTextureAtlasData;
         /**
          * @private
          */
