b755e3c102f1e7b48e194ba21f18127d8295a017
himuil <jerry.lin@egret.com>
2017-12-04 10:48:14 +0800

    Upgrade to 5.0.14

--- src/egret/system/Capabilities.ts
+++ src/egret/system/Capabilities.ts
@@ -247,7 +247,7 @@ namespace egret {
          * @language zh_CN
          */
         public static get engineVersion():string {
-            return "5.0.13";
+            return "5.0.14";
         }
 
         /**
