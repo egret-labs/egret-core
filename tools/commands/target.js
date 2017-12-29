e5902e3391b02f3e7a6280c61b3e6d53d07f2a0f
WanderWang <185713114@qq.com>
2017-12-29 13:31:22 +0800

    update

--- tools/commands/target.js
+++ tools/commands/target.js
@@ -35,6 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var path = require("path");
 var FileUtil = require("../lib/FileUtil");
+var utils_1 = require("../lib/utils");
 var Target = (function () {
     function Target() {
     }
@@ -46,6 +47,7 @@ var Target = (function () {
                     case 0:
                         option = egret.args;
                         options = parseCommandLine();
+                        utils_1.checkEgret();
                         projectName = path.basename(option.projectDir);
                         return [4 /*yield*/, getTargetTemplateConfig()];
                     case 1:
@@ -63,7 +65,12 @@ var Target = (function () {
                                 var content = FileUtil.read(filepath);
                                 var value = options[arg.name];
                                 if (!value) {
-                                    throw "\u9700\u8981\u4F20\u9012\u53C2\u6570:--" + arg.name;
+                                    if (arg.default) {
+                                        value = arg.default;
+                                    }
+                                    else {
+                                        throw "\u9700\u8981\u4F20\u9012\u53C2\u6570:--" + arg.name;
+                                    }
                                 }
                                 var reg = new RegExp("{" + arg.name + "}", "gi");
                                 content = content.replace(reg, value);
