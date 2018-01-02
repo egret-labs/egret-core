53385a539cd8e8be284f7966c1991967326aaa25
himuil <jerry.lin@egret.com>
2018-01-02 10:39:09 +0800

    Modify version

--- build/egret/egret.js
+++ build/egret/egret.js
@@ -17167,7 +17167,7 @@ var egret;
              * @language zh_CN
              */
             get: function () {
-                return "5.1.1";
+                return "5.1.2";
             },
             enumerable: true,
             configurable: true
