43d2feb5da90d06924f362f44001b83ec364f600
WanderWang <185713114@qq.com>
2018-01-15 14:03:23 +0800

    add eui support for bricks

--- tools/templates/empty/scripts/config.ts
+++ tools/templates/empty/scripts/config.ts
@@ -43,6 +43,7 @@ const config: ResourceManagerConfig = {
                 outputDir,
                 commands: [
                     new CompilePlugin({ libraryType: "debug" }),
+                    new ExmlPlugin('commonjs'), // 非 EUI 项目关闭此设置
                     new ManifestPlugin({ output: 'manifest.json' })
                 ]
             }
