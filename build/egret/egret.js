63be87bfbdefd622d35b3a5bb297423c0a5358cf
himuil <jerry.lin@egret.com>
2018-01-15 15:23:06 +0800

    Upgrade to 5.1.3

--- build/egret/egret.js
+++ build/egret/egret.js
@@ -16921,7 +16921,7 @@ var egret;
              * @language zh_CN
              */
             get: function () {
-                return "5.1.2";
+                return "5.1.3";
             },
             enumerable: true,
             configurable: true
