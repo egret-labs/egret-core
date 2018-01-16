b5780c9b5c49116e34fb3c36e1127631c4a46754
WanderWang <185713114@qq.com>
2018-01-16 15:02:25 +0800

    add bricks script

---
+++ tools/templates/empty/scripts/bricks/bricks.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+type ManifestConfig = {
+
+    initial: string[],
+
+    game: string[]
+
+}
+
+export class BricksPlugin implements plugins.Command {
+
+    constructor() {
+    }
+    async onFile(file: plugins.File) {
+        const filename = file.origin;
+        if (filename == 'manifest.json') {
+            const contents = file.contents.toString();
+            const jsonData: ManifestConfig = JSON.parse(contents);
+
+            let content = '';
+
+            for (let item of jsonData.initial) {
+                if (item != 'js/promise.js' && item != 'js/promise.min.js') {
+                    content += `BK.script.loadLib("GameRes://${item}");\n`
+                }
+                if (item == "js/egret.js" || item == 'js/egret.min.js') {
+                    content += `BK.script.loadLib("GameRes://egret.bricks.js");\n`
+                }
+            }
+            for (let item of jsonData.game) {
+                content += `BK.script.loadLib("GameRes://${item}");\n`
+            }
+            file.path = file.dirname + '/manifest.js'
+            file.contents = new Buffer(content);
+        }
+        return file;
+    }
+    async onFinish(pluginContext) {
+
+    }
+}
