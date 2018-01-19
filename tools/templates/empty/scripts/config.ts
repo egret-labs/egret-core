dd6674f06ff52c1194e64899dec7dbd0268b11dd
WanderWang <185713114@qq.com>
2018-01-19 16:28:09 +0800

    update versions

--- tools/templates/empty/scripts/config.ts
+++ tools/templates/empty/scripts/config.ts
@@ -36,7 +36,7 @@ const config: ResourceManagerConfig = {
                 return {
                     outputDir,
                     commands: [
-                        new CompilePlugin({ libraryType: "release" }),
+                        new CompilePlugin({ libraryType: "release", defines: { DEBUG: false, RELEASE: true } }),
                         new ExmlPlugin('commonjs'), // 非 EUI 项目关闭此设置
                         new WxgamePlugin(),
                         new UglifyPlugin([{
