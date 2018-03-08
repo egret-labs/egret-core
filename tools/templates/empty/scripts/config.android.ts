c052f026aa4566ddc29328178962f0912d35b53e
himuil <jerry.lin@egret.com>
2018-03-08 17:04:58 +0800

    Add iOS and Android project config

---
+++ tools/templates/empty/scripts/config.android.ts
@@ -0,0 +1,38 @@
+/// 阅读 api.d.ts 查看文档
+///<reference path="api.d.ts"/>
+
+import * as path from 'path';
+import { UglifyPlugin, CompilePlugin, ManifestPlugin, ExmlPlugin, EmitResConfigFilePlugin, TextureMergerPlugin } from 'built-in';
+import { BricksPlugin } from './bricks/bricks';
+import { CustomPlugin } from './myplugin';
+import * as defaultConfig from './config';
+
+const config: ResourceManagerConfig = {
+
+    buildConfig: (params) => {
+
+        const { target, command, projectName, version } = params;
+        const outputDir = `../${projectName}_android/assets/game`;
+        return {
+            outputDir,
+            commands: [
+                // new CompilePlugin({ libraryType: "debug", defines: { DEBUG: false, RELEASE: true } }),
+                new ExmlPlugin('commonjs'), // 非 EUI 项目关闭此设置
+                new CompilePlugin({ libraryType: "release", defines: { DEBUG: false, RELEASE: true } }),
+                new UglifyPlugin([{
+                    sources: ["main.js"],
+                    target: "main.min.js"
+                }]),
+                new ManifestPlugin({ output: 'manifest.json' })
+            ]
+        }
+    },
+
+    mergeSelector: defaultConfig.mergeSelector,
+
+    typeSelector: defaultConfig.typeSelector
+}
+
+
+
+export = config;
