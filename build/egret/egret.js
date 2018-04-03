ad8ac3df9e00fa1e9d137dc31a4b08edd6dca196
himuil <jerry.lin@egret.com>
2018-04-03 16:57:55 +0800

    Upgrade to 5.1.9

--- build/egret/egret.js
+++ build/egret/egret.js
@@ -16887,7 +16887,7 @@ var egret;
          * @platform Web,Native
          * @language zh_CN
          */
-        Capabilities.engineVersion = "5.1.8";
+        Capabilities.engineVersion = "5.1.9";
         /***
          * current render mode.
          * @type {string}
