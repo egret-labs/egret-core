466dbc79e0b49759b42b886a6706902ec62d30cf
王恒尊 <982049377@qq.com>
2018-06-26 15:43:24 +0800

    Specification code

--- tools/lib/eui/JsonParserFactory.ts
+++ tools/lib/eui/JsonParserFactory.ts
@@ -9,14 +9,12 @@ var __extends = this && this.__extends || function __extends(t, e) {
     r.prototype = e.prototype, t.prototype = new r();
 };
 class JSONParseClass {
-    private json = {};
+    private json;
     private skinClass = {};
     private target;
     private skinName: string;//一个skin可能包含多个
-    private skinNameList: string[] = [];//因为是单例，操作的都是一个解析，所以这个保存皮肤
-    private targetList: any[] = [];//因为是单例，操作的都是一个解析，所以这个保存对象池
 
-    private euiNormalize = {
+    private euiNormalizeNames = {
         "$eBL": "eui.BitmapLabel",
         "$eB": "eui.Button",
         "$eCB": "eui.CheckBox",
@@ -76,7 +74,6 @@ class JSONParseClass {
         for (let exml in skinMap) {
             let skinData = skinMap[exml];
             if (!skinData) continue;
-
             let paths = exml.split(".");
             let target = window;
             for (let p of paths) {
@@ -90,7 +87,7 @@ class JSONParseClass {
                 }
             }
 
-            let superName = this.euiNormalize[skinData["$sC"]] == undefined ? skinData["$sC"] : this.euiNormalize[skinData["$sC"]];
+            let superName = this.euiNormalizeNames[skinData["$sC"]] == undefined ? skinData["$sC"] : this.euiNormalizeNames[skinData["$sC"]];
             skinResult[exml] = target[paths[paths.length - 1]] = this.generateSkinClass(skinData, exml, superName);
             if (skinMap[exml]["$path"]) {
                 generateEUI2["paths"][skinMap[exml]["$path"]] = skinResult[exml];
@@ -101,22 +98,27 @@ class JSONParseClass {
 
 
     create(skinName: string, target: any) {
+        if (!this.json) {
+            console.log("Missing json defined by eui resource, please modify the theme adapter");
+            console.log("缺少eui资源定义的json，请修改主题适配器");
+            return;
+        }
         /** 先解析对应名字的的 */
         this.target = target;
         this.skinName = skinName;
         this.skinClass = this.json[skinName];
         //开始生成
-        this.createBase();
-        this.creatSkinParts();
-        this.creatState();
-        this.creatBinding();
+        this.applyBase();
+        this.applySkinParts();
+        this.applyState();
+        this.applyBinding();
         //skinParts 只能最后赋值，在comp中引用问题
         if (this.skinClass["$sP"] == undefined)
             this.target["skinParts"] = [];
         else
             this.target["skinParts"] = this.skinClass["$sP"]
     }
-    private creatSkinParts() {
+    private applySkinParts() {
         if (this.skinClass["$sP"] == undefined)
             return;
         for (let component of this.skinClass["$sP"]) {
@@ -124,37 +126,37 @@ class JSONParseClass {
                 this.createElementContentOrViewport(component);
         }
     }
-    private createBase() {
+    private applyBase() {
         if (this.skinClass["$bs"] == undefined) return;
         this.addCommonProperty("$bs", this.target);
     }
     private createElementContentOrViewport(component: string) {
-        let temp;
-        let typestr = this.getNormalizeEui(this.skinClass[component].$t);
-        if (typestr == "egret.tween.TweenGroup") {
-            temp = this.creatsEgretTweenGroup(component);
+        let result;
+        const typeStr = this.getNormalizeEuiName(this.skinClass[component].$t);
+        if (typeStr == "egret.tween.TweenGroup") {
+            result = this.creatsEgretTweenGroup(component);
         } else {
             /** 有可能对象是从外面一定义的皮肤 */
-            let type = egret.getDefinitionByName(typestr);
+            const type = egret.getDefinitionByName(typeStr);
             this.$createNewObject(() => {
-                temp = new type();
+                result = new type();
             })
-            this.addCommonProperty(component, temp);
+            this.addCommonProperty(component, result);
         }
-        this.target[component] = temp;
-        return temp;
+        this.target[component] = result;
+        return result;
     }
     /**
      * 生成单位，有可能会跳出当前皮肤，所以统一维护target和skin
      * @param callback 创建对象的真实逻辑 
      */
     private $createNewObject(callback: Function) {
-        this.skinNameList.push(this.skinName);
-        this.targetList.push(this.target);
+        const skinName = this.skinName;
+        const target = this.target;
         callback();
-        this.skinName = this.skinNameList.pop();
+        this.skinName = skinName;
         this.skinClass = this.json[this.skinName];
-        this.target = this.targetList.pop();
+        this.target = target;
     }
 
     /**
@@ -162,152 +164,157 @@ class JSONParseClass {
      * @param component 名字索引 
      */
     private creatsEgretTweenGroup(component: string) {
-        let temp = this.createTypeObject(component);
+        let result = this.createTypeObject(component);
         let items = [];
         for (let item of this.skinClass[component]["items"]) {
             items.push(this.createEgretTweenItem(item));
         }
-        temp["items"] = items;
-        return temp;
+        result["items"] = items;
+        return result;
     }
     /**
      * 生成对应的缓动单位
      * @param tweenItem 名字索引
      */
     private createEgretTweenItem(tweenItem: string) {
-        let temp = this.createTypeObject(tweenItem);
+        let result = this.createTypeObject(tweenItem);
         let paths = [];
-        for (let proper in this.skinClass[tweenItem]) {
-            if (proper == "$t" || proper == "target") {
+        for (let prop in this.skinClass[tweenItem]) {
+            const property = this.skinClass[tweenItem][prop];
+            if (prop == "$t" || prop == "target") {
             }
-            else if (proper == "paths") {
-                for (let path of this.skinClass[tweenItem][proper])
+            else if (prop == "paths") {
+                for (let path of property)
                     paths.push(this.createSetOrTo(path));
-            } else if (proper == "target") {
+            } else if (prop == "target") {
                 this.$createNewObject(() => {
-                    temp[proper] = this.createElementContentOrViewport(this.skinClass[tweenItem][proper]);
-                    this.target[this.skinClass[tweenItem][proper]] = temp[proper];
+                    result[prop] = this.createElementContentOrViewport(property);
+                    this.target[property] = result[prop];
                 })
             } else {
-                temp[proper] = this.skinClass[tweenItem][proper];
+                result[prop] = property;
             }
         }
-        temp["paths"] = paths;
-        this.target[tweenItem] = temp;
-        return temp;
+        result["paths"] = paths;
+        this.target[tweenItem] = result;
+        return result;
     }
 
     private createSetOrTo(key: string) {
-        let temp = this.createTypeObject(key);
-        for (let proper in this.skinClass[key]) {
-            if (proper == "$t" || proper == "target") {
-
+        let result = this.createTypeObject(key);
+        for (let prop in this.skinClass[key]) {
+            const property = this.skinClass[key][prop];
+            if (prop == "$t" || prop == "target") {
             }
-            else if (proper == "props") {
-                temp[proper] = this.createEgretObject(this.skinClass[key][proper]);
-                this.target[this.skinClass[key][proper]] = temp[proper];
-            } else {
-                temp[proper] = this.skinClass[key][proper];
+            else if (prop == "props") {
+                result[prop] = this.createObject(property);
+                this.target[property] = result[prop];
+            }
+            else {
+                result[prop] = property;
             }
         }
-        return temp;
+        return result;
     }
-    private createEgretObject(name: string) {
-        let temp = {};
+    private createObject(name: string) {
+        let result = {};
         for (let prop in this.skinClass[name]) {
             if (prop == "$t" || prop == "target") {
             }
-            else
-                temp[prop] = this.skinClass[name][prop];
+            else {
+                result[prop] = this.skinClass[name][prop];
+            }
         }
-        return temp;
+        return result;
     }
 
-    private addCommonProperty(component: string, target: any) {
-        let eleC: string[] = [];
-        let sId: string[] = [];
-        for (let property in this.skinClass[component]) {
-            if (property == "$t") {
+    private addCommonProperty(componentName: string, target: any) {
+        let eleC: string[];
+        let sId: string[];
+        for (let prop in this.skinClass[componentName]) {
+            let property = this.skinClass[componentName][prop];
+            if (prop == "$t") {
             }
-            else if (property == "layout") {
-                let layout = this.skinClass[component][property];
-                target["layout"] = this.createLayout(layout);
+            else if (prop == "layout") {
+                target[prop] = this.createLayout(property);
             }
-            else if (property == "$eleC") {
-                eleC = this.skinClass[component]["$eleC"];
+            else if (prop == "$eleC") {
+                eleC = property;
             }
-            else if (property == "$sId") {
-                sId = this.skinClass[component]["$sId"];
+            else if (prop == "$sId") {
+                sId = property;
             }
-            else if (property == "scale9Grid") {
-                target[property] = this.getScale9Grid(this.skinClass[component][property])
+            else if (prop == "scale9Grid") {
+                target[prop] = this.getScale9Grid(property);
             }
-            else if (property == "skinName") {
+            else if (prop == "skinName") {
                 this.$createNewObject(() => {
-                    target[property] = this.skinClass[component][property];
-                })
-            } else if (property == "itemRendererSkinName") {
+                    target[prop] = property;
+                });
+            }
+            else if (prop == "itemRendererSkinName") {
                 this.$createNewObject(() => {
-                    let dirPath = this.skinClass[component][property].split(".");
+                    let dirPath = property.split(".");
                     let t = window;
                     for (let p of dirPath) {
                         t = t[p];
                     }
-                    target[property] = t;
-                })
-            } else if (property == "itemRenderer") {
-                target[property] = egret.getDefinitionByName(this.skinClass[component][property]);
+                    target[prop] = t;
+                });
             }
-            else if (property == "dataProvider") {
-                target[property] = this.createDataProvider(this.skinClass[component][property]);
+            else if (prop == "itemRenderer") {
+                target[prop] = egret.getDefinitionByName(property);
             }
-            else if (property == "viewport") {
-                let viewport = this.skinClass[component][property];
-                target["viewport"] = this.createElementContentOrViewport(viewport);
+            else if (prop == "dataProvider") {
+                target[prop] = this.createDataProvider(property);
             }
-            else
-                target[property] = this.skinClass[component][property];
+            else if (prop == "viewport") {
+                target[prop] = this.createElementContentOrViewport(property);
+            }
+            else {
+                target[prop] = property;
+            }
         }
         let ele = []
-        if (eleC.length > 0) {
+        if (eleC && eleC.length > 0) {
             for (let element of eleC) {
                 let e = this.createElementContentOrViewport(element);
                 ele.push(e);
             }
         }
-        if (sId.length > 0) {
+        target["elementsContent"] = ele;
+        if (sId && sId.length > 0) {
             for (let element of sId) {
                 this.createElementContentOrViewport(element);
             }
         }
-        target["elementsContent"] = ele;
         return target;
     }
 
-    private createLayout(component: string) {
-        let temp = this.createTypeObject(component);
-        for (let property in this.skinClass[component]) {
-            if (property == "$t") {
-            } else
-                temp[property] = this.skinClass[component][property];
+    private createLayout(componentName: string) {
+        let result = this.createTypeObject(componentName);
+        const component = this.skinClass[componentName];
+        for (let property in component) {
+            if (property !== "$t") {
+                result[property] = component[property];
+            }
         }
-        this.target[component] = temp;
-        return temp;
+        this.target[componentName] = result;
+        return result;
     }
 
-    private creatState() {
+    private applyState() {
         if (this.skinClass["$s"] == undefined) return;
-        let setProperty = [];
         let states = [];
         for (let state in this.skinClass["$s"]) {
             let setProperty = [];
             let tempState = this.skinClass["$s"][state];
-            if (this.skinClass["$s"][state]["$saI"]) {
+            if (tempState["$saI"]) {
                 for (let property of tempState["$saI"]) {
                     setProperty.push(new eui.AddItems(property["target"], property["property"], property["position"], property["relativeTo"]));
                 }
             }
-            if (this.skinClass["$s"][state]["$ssP"]) {
+            if (tempState["$ssP"]) {
                 for (let property of tempState["$ssP"]) {
                     if (property["name"]) {
                         let value = property["value"];
@@ -324,45 +331,46 @@ class JSONParseClass {
         }
         this.target["states"] = states;
     }
-    private creatBinding() {
+    private applyBinding() {
         if (this.skinClass["$b"] == undefined) return;
         for (let bindingDate of this.skinClass["$b"]) {
-            if (bindingDate["$bc"] !== undefined)
+            if (bindingDate["$bc"] !== undefined) {
                 eui.Binding.$bindProperties(this.target, bindingDate["$bd"], bindingDate["$bc"], this.target[bindingDate["$bt"]], bindingDate["$bp"]);
-            else
+            }
+            else {
                 eui.Binding.bindProperty(this.target, bindingDate["$bd"][0].split("."), this.target[bindingDate["$bt"]], bindingDate["$bp"])
+            }
         }
     }
     private createDataProvider(component: string) {
         if (component == "") return undefined;
-        let temp = this.createTypeObject(component);
+        let result = this.createTypeObject(component);
         let source = [];
         for (let sour of this.skinClass[component]["source"]) {
-            source.push(this.createdataItem(sour));
+            source.push(this.createItemRender(sour));
         }
-        temp["source"] = source;
-        return temp;
+        result["source"] = source;
+        return result;
     }
-    private createdataItem(itemName: string) {
-        let temp = this.createTypeObject(itemName);
+    private createItemRender(itemName: string) {
+        let result = this.createTypeObject(itemName);
         for (let property in this.skinClass[itemName]) {
-            if (property == "$t") {
-            } else {
-                temp[property] = this.skinClass[itemName][property];
+            if (property != "$t") {
+                result[property] = this.skinClass[itemName][property];
             }
         }
-        return temp;
+        return result;
     }
-    private getNormalizeEui(str: string) {
-        return this.euiNormalize[str] == undefined ? str : this.euiNormalize[str];;
+    private getNormalizeEuiName(str: string):string {
+        return this.euiNormalizeNames[str] ?  this.euiNormalizeNames[str]:str;
     }
     private createTypeObject(component: string) {
-        let typestr = this.getNormalizeEui(this.skinClass[component].$t);
-        let type = egret.getDefinitionByName(typestr);
+        const typestr = this.getNormalizeEuiName(this.skinClass[component].$t);
+        const type = egret.getDefinitionByName(typestr);
         return new type();
     }
     private getScale9Grid(data: string) {
-        let datalist = data.split(",");
+        const datalist = data.split(",");
         return new egret.Rectangle(parseFloat(datalist[0]), parseFloat(datalist[1]), parseFloat(datalist[2]), parseFloat(datalist[3]));
     }
 }
