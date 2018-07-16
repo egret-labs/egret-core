44119a09a413d064fa5a1333e3d9de05d2694b54
himuil <jerry.lin@egret.com>
2018-07-16 13:44:32 +0800

    Fix filter bug

--- build/egret/egret.js
+++ build/egret/egret.js
@@ -1988,6 +1988,16 @@ var egret;
                     }
                     else {
                         self.$updateRenderMode();
+                        var p = self.$parent;
+                        if (p && !p.$cacheDirty) {
+                            p.$cacheDirty = true;
+                            p.$cacheDirtyUp();
+                        }
+                        var maskedObject = self.$maskedObject;
+                        if (maskedObject && !maskedObject.$cacheDirty) {
+                            maskedObject.$cacheDirty = true;
+                            maskedObject.$cacheDirtyUp();
+                        }
                     }
                     return;
                 }
