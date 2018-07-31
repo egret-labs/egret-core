db5a4de3fd3f2b61c3fcf6fcae88d3856021683e
himuil <jerry.lin@egret.com>
2018-07-31 11:29:21 +0800

    Upgrade to 5.2.6

--- src/egret/system/Capabilities.ts
+++ src/egret/system/Capabilities.ts
@@ -231,7 +231,7 @@ namespace egret {
          * @platform Web,Native
          * @language zh_CN
          */
-        public static readonly engineVersion:string = "5.2.5";
+        public static readonly engineVersion:string = "5.2.6";
 
         /***
          * current render mode.
