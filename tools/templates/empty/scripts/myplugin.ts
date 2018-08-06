39ec27a7a29c0a7882b0fa45761554bf1ecef8a9
himuil <jerry.lin@egret.com>
2018-08-06 10:00:06 +0800

    Modify docs

--- tools/templates/empty/scripts/myplugin.ts
+++ tools/templates/empty/scripts/myplugin.ts
@@ -1,5 +1,5 @@
 /**
- * 示例自定义插件，您可以查阅 http://developer.egret.com/cn/2d/projectConfig/cmdExtensionPluginin/ 
+ * 示例自定义插件，您可以查阅 http://developer.egret.com/cn/github/egret-docs/Engine2D/projectConfig/cmdExtensionPlugin/index.html
  * 了解如何开发一个自定义插件
  */
 export class CustomPlugin implements plugins.Command {
