326fa74b5d3ea477687e57ca84c1259e258f1e27
himuil <jerry.lin@egret.com>
2018-08-30 11:46:26 +0800

    Modify CanvasRenderer

--- build/egret/egret.js
+++ build/egret/egret.js
@@ -15880,13 +15880,12 @@ var egret;
                     var maskMatrix = egret.Matrix.create();
                     maskMatrix.copyFrom(mask.$getConcatenatedMatrix());
                     mask.$getConcatenatedMatrixAt(displayObject, maskMatrix);
-                    context.transform(1, 0, 0, 1, offsetX, offsetY);
+                    maskMatrix.prepend(1, 0, 0, 1, offsetX, offsetY);
                     context.transform(maskMatrix.a, maskMatrix.b, maskMatrix.c, maskMatrix.d, maskMatrix.tx, maskMatrix.ty);
                     var calls = this.drawDisplayObject(mask, context, 0, 0);
                     this.renderingMask = false;
                     maskMatrix.$invertInto(maskMatrix);
                     context.transform(maskMatrix.a, maskMatrix.b, maskMatrix.c, maskMatrix.d, maskMatrix.tx, maskMatrix.ty);
-                    context.transform(1, 0, 0, 1, -offsetX, -offsetY);
                     egret.Matrix.release(maskMatrix);
                     if (scrollRect) {
                         context.beginPath();
