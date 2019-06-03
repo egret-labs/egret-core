0e67f987112ec0e81054f3cab8aff76e54beab3b
yang hang <1220826563@qq.com>
2019-06-03 17:06:35 +0800

    fix: the old problems arise again due to my operational errors, and need to be corrected, otherwise, wechat will have problems.

--- src/egret/web/rendering/webgl/WebGLRenderContext.ts
+++ src/egret/web/rendering/webgl/WebGLRenderContext.ts
@@ -578,7 +578,10 @@ namespace egret.web {
                     }
                 }
                 if (bitmapData.$deleteSource && bitmapData.webGLTexture) {
-                    bitmapData.source = null;
+                    if (bitmapData.source) {
+                        bitmapData.source.src = '';
+                        bitmapData.source = null;
+                    }
                     bitmapData.clearCompressedTextureData();
                 }
                 if (bitmapData.webGLTexture) {
