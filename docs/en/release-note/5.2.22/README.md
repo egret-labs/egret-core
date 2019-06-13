cd31acdff64eb53fdc89309bdcb7ca2dd79928bf
张宇 <runinspring@gmail.com>
2019-06-13 15:25:43 +0800

    `RenamePlugin` increase the settings callback function to get information about the file

--- docs/en/release-note/5.2.22/README.md
+++ docs/en/release-note/5.2.22/README.md
@@ -4,3 +4,4 @@
 ## 2D Rendering - JavaScript 
 - **[fix]** Fix an error in destroying `htmlSound` audio when it was initially loaded
 - **[optimize]** In Safari browser on Mac, use web audio
+- **[optimize]** `RenamePlugin` increase the settings callback function to get information about the file
--- tools/tasks/rename.js
+++ tools/tasks/rename.js
@@ -13,8 +13,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
         while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
                 case 0: case 1: t = op; break;
                 case 4: _.label++; return { value: op[1], done: false };
@@ -61,6 +61,14 @@ var RenamePlugin = /** @class */ (function () {
                         p = path.dirname(file.origin) + "/";
                         toFilename = match.to.replace('[name]', name).replace('[hash]', hash).replace('[ext]', extname).replace("[path]", p);
                         file.path = file.base + '/' + toFilename;
+                        if (this.options.callback) {
+                            this.options.callback({
+                                name: name,
+                                extname: extname,
+                                hash: hash,
+                                origin: p
+                            });
+                        }
                         if (this.options.verbose) {
                             console.log("RenamePlugin: " + file.origin + " => " + toFilename);
                         }
