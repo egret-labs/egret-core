5db7eae0a7b4d04df78e0b7b6a57612779949cdc
张宇 <runinspring@gmail.com>
2019-06-13 10:37:01 +0800

    update engineVersion

--- src/egret/system/Capabilities.ts
+++ src/egret/system/Capabilities.ts
@@ -271,7 +271,7 @@ namespace egret {
          * @platform Web,Native
          * @language zh_CN
          */
-        public static readonly engineVersion: string = "5.2.21";
+        public static readonly engineVersion: string = "5.2.22";
 
         /***
          * current render mode.
