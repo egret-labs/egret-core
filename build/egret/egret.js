8a0e821626548e5704973aceb4bd575412481b10
张宇 <runinspring@gmail.com>
2019-09-25 14:05:19 +0800

    make

--- build/egret/egret.js
+++ build/egret/egret.js
@@ -18033,7 +18033,7 @@ var egret;
          * @platform Web,Native
          * @language zh_CN
          */
-        Capabilities.engineVersion = "5.2.28";
+        Capabilities.engineVersion = "5.2.29";
         /***
          * current render mode.
          * @type {string}
